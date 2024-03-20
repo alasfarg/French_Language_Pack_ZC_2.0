6fa0dbf0fc94230bf51da63a8590017bdf066784
andre <andre.lasfargues@iut-rodez.fr>
2024-03-20 18:19:41 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.checkout_shipping.php
@@ -0,0 +1,23 @@
+<?php
+$define = [
+    'NAVBAR_TITLE_1' => 'Encaissement',
+    'NAVBAR_TITLE_2' => 'Méthode de livraison',
+    'HEADING_TITLE' => 'Étape 1 sur 3 - Informations de livraison',
+    'TABLE_HEADING_SHIPPING_ADDRESS' => 'Adresse de livraison',
+    'TEXT_CHOOSE_SHIPPING_DESTINATION' => 'Votre commande sera livrée à l\'adresse à gauche ou vous pouvez modifier l\'adresse de livraison en cliquant sur le bouton <em>Modifier l\'adresse</em>.',
+    'TITLE_SHIPPING_ADDRESS' => 'Adresse de livraison: ',
+    'TABLE_HEADING_SHIPPING_METHOD' => 'Méthode de livraison',
+    'TEXT_CHOOSE_SHIPPING_METHOD' => 'Veuillez choisir la méthode de livraison préférée à utiliser pour cette commande.',
+    'TITLE_PLEASE_SELECT' => 'Veuillez choisir',
+    'TEXT_ENTER_SHIPPING_INFORMATION' => 'C\'est actuellement la seule méthode de livraison disponible pour cette commande.',
+    'TITLE_NO_SHIPPING_AVAILABLE' => 'Non disponible actuellement',
+    'TEXT_NO_SHIPPING_AVAILABLE' => '<span class="alert">Désolés, nous ne livrons pas votre région actuellement.</span><br />Veuillez nous contacter pour des mesures alternatives.',
+    'TABLE_HEADING_COMMENTS' => 'Instructions spéciales ou commentaires sur votre commande',
+    'TITLE_CONTINUE_CHECKOUT_PROCEDURE' => 'Continuer vers l\'étape 2',
+    'TEXT_CONTINUE_CHECKOUT_PROCEDURE' => '- Choisir votre méthode de paiement.',
+    'FREE_SHIPPING_TITLE' => 'Livraison gratuite',
+    'FREE_SHIPPING_DESCRIPTION' => 'Livraison gratuite pour les commandes d\'un montant supérieur à %s',
+    'ERROR_PLEASE_RESELECT_SHIPPING_METHOD' => 'Vos options disponibles de livraison ont changé. Veuillez re-sélectionner votre méthode de livraison souhaitée.',
+];
+
+return $define;
