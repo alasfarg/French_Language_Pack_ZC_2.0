00fc117d36e5e5501d70cb06a91546dbc1e7811a
andre <andre.lasfargues@iut-rodez.fr>
2024-03-20 18:15:35 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.account_history_info.php
@@ -0,0 +1,26 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Mon compte',
+    'NAVBAR_TITLE_1' => 'Mon compte',
+    'NAVBAR_TITLE_2' => 'Historique',
+    'NAVBAR_TITLE_3' => 'Commande #%s',
+    'HEADING_TITLE' => 'Informations de commande',
+    'HEADING_ORDER_NUMBER' => 'Commande #%s',
+    'HEADING_ORDER_DATE' => 'Date commande : ',
+    'HEADING_DELIVERY_ADDRESS' => 'Adresse de livraison',
+    'HEADING_SHIPPING_METHOD' => 'Méthode de livraison',
+    'HEADING_PRODUCTS' => 'Produits',
+    'HEADING_TAX' => 'Taxes',
+    'HEADING_TOTAL' => 'Total',
+    'HEADING_QUANTITY' => 'Qté',
+    'HEADING_BILLING_ADDRESS' => 'Adresse de facturation',
+    'HEADING_PAYMENT_METHOD' => 'Méthode de paiement',
+    'HEADING_ORDER_HISTORY' => 'Historique des états &amp; commentaires',
+    'TABLE_HEADING_STATUS_DATE' => 'Date',
+    'TABLE_HEADING_STATUS_ORDER_STATUS' => 'État Commande',
+    'TABLE_HEADING_STATUS_COMMENTS' => 'Commentaires',
+    'QUANTITY_SUFFIX' => '&nbsp;ea.  ',
+    'ORDER_HEADING_DIVIDER' => '&nbsp;-&nbsp;',
+];
+
+return $define;
