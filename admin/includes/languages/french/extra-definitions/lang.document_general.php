aba6c757ae8a2b5ddf458b1082a44a5a555dacf1
andre <andre.lasfargues@iut-rodez.fr>
2024-03-20 16:40:54 +0100

    ajout <filename>

---
+++ admin/includes/languages/french/extra-definitions/lang.document_general.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Zcwilt 2020 Jun 02 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'TEXT_DOCUMENT_STATUS' => 'Statut du document:',
+    'TEXT_DOCUMENT_DATE_AVAILABLE' => 'Date Disponible:',
+    'TEXT_DOCUMENT_AVAILABLE' => 'Disponible',
+    'TEXT_DOCUMENT_NAME' => 'Nom du Document:',
+    'TEXT_DOCUMENT_DETAILS' => 'Contenu du Document:',
+    'TEXT_DOCUMENT_IMAGE' => 'Image du Document:',
+    'TEXT_DOCUMENT_IMAGE_DIR' => 'Télécharger dans le répertoire:',
+    'TEXT_DOCUMENT_URL' => 'URL du Document:',
+    'TEXT_DOCUMENT_URL_WITHOUT_HTTP' => '<small>(sans http://)</small>',
+];
+
+return $define;
