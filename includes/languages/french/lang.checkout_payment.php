44be515e9a3642522b429161f97eec8a3de01cc5
andre <andre.lasfargues@iut-rodez.fr>
2024-03-20 18:17:28 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.checkout_payment.php
@@ -0,0 +1,22 @@
+<?php
+$define = [
+    'NAVBAR_TITLE_1' => 'Encaissement - Étape 1',
+    'NAVBAR_TITLE_2' => 'Méthode de paiement - Étape 2',
+    'HEADING_TITLE' => 'Étape 2 sur 3 - Informations de paiement',
+    'TABLE_HEADING_BILLING_ADDRESS' => 'Adresse de facturation',
+    'TEXT_SELECTED_BILLING_DESTINATION' => 'Votre adresse de facturation est affichée à gauche. L\'adresse de facturation doit correspondre à celle sur votre carte de crédit. Vous pouvez modifier l\'adresse de facturation en cliquant sur le bouton <em>Modifier l\'adresse</em>.',
+    'TITLE_BILLING_ADDRESS' => 'Adresse de facturation : ',
+    'TABLE_HEADING_PAYMENT_METHOD' => 'Méthode de paiement',
+    'TEXT_SELECT_PAYMENT_METHOD' => 'Veuillez choisir une méthode de paiement pour votre commande.',
+    'TITLE_PLEASE_SELECT' => 'Veuillez choisir',
+    'TABLE_HEADING_COMMENTS' => 'Instructions spéciales ou commentaires de commande',
+    'TITLE_NO_PAYMENT_OPTIONS_AVAILABLE' => '<span class="alert">Désolé, nous n\'acceptons pas les paiements de votre région pour le moment. </span> <br /> Veuillez nous contacter pour d\'autres arrangements.',
+    'TITLE_CONTINUE_CHECKOUT_PROCEDURE' => '<strong>Continuez vers l\'étape 3</strong>',
+    'TEXT_CONTINUE_CHECKOUT_PROCEDURE' => '- pour confirmer votre commande.',
+    'TABLE_HEADING_CONDITIONS' => '<span class="termsconditions">Conditions Générales</span>',
+    'TEXT_CONDITIONS_DESCRIPTION' => '<span class="termsdescription">En cochant la case suivante, vous déclarez accepter nos Conditions Générales qui s\'appliquent à cette commande. Vous pouvez en prendre connaissance <a href="'.zen_href_link(FILENAME_CONDITIONS,'','SSL',
+    'TEXT_CONDITIONS_CONFIRM' => '<span class="termsiagree">Je déclare avoir pris connaissance et accepté les Conditions Générales qui s\'appliquent à cette commande.</span>',
+    'TEXT_YOUR_TOTAL' => 'Votre total',
+];
+
+return $define;
