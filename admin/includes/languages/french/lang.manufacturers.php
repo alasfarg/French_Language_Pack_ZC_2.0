40f13b946eeaa47d4cacf88cbca998eab42d430e
andre <andre.lasfargues@iut-rodez.fr>
2024-03-20 19:36:15 +0100

    ajout fichiers corrigés

---
+++ admin/includes/languages/french/lang.manufacturers.php
@@ -0,0 +1,29 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Fabricants',
+    'TABLE_HEADING_MANUFACTURERS' => 'Fabricants',
+    //'TABLE_HEADING_ACTION' => 'Action',
+    'TEXT_HEADING_NEW_MANUFACTURER' => 'Nouveau fabricant',
+    'TEXT_HEADING_EDIT_MANUFACTURER' => 'Éder fabricant',
+    'TEXT_HEADING_DELETE_MANUFACTURER' => 'Supprimer fabricant',
+    'TEXT_MANUFACTURERS' => 'Fabricants :',
+    //'TEXT_DATE_ADDED' => 'Date de création :',
+    //'TEXT_LAST_MODIFIED' => 'Denière Modification :',
+    //'TEXT_PRODUCTS' => 'Produits :',
+    //'TEXT_PRODUCTS_IMAGE_DIR' => 'Uploader vers le répertoire :',
+    //'TEXT_MANUFACTURERS_IMAGE_MANUAL' => '<strong>Ou, sélectionnez une image existante à partir du serveur, nom du fichier :</strong>',
+    'TEXT_NEW_INTRO' => 'Veuillez renseigner les informations concernant le nouveau fabricant',
+    //'TEXT_EDIT_INTRO' => 'Veuillez effectuer les changements nécessaires',
+    'TEXT_MANUFACTURERS_NAME' => 'Nom du fabricant :',
+    'TEXT_MANUFACTURERS_IMAGE' => 'Image du fabricant :',
+    'TEXT_MANUFACTURERS_URL' => 'URL du fabricant :',
+    'TEXT_DELETE_INTRO' => 'Êtes-vous certain(e) de vouloir supprimer ce fabricant ?',
+    //'TEXT_DELETE_IMAGE' => 'Supprimer l\'image du fabricant ?',
+    'TEXT_DELETE_PRODUCTS' => 'Supprimer les produits de ce fabricant ? (en incluant les avis, les promotions et les produits attendus)',
+    'TEXT_DELETE_WARNING_PRODUCTS' => '<b>ATTENTION :</b> Il y a %s produits encore associé(s) à ce fabricant !',
+    'TABLE_HEADING_MANUFACTURER_FEATURED' => 'En vedette?',
+    'TEXT_MANUFACTURER_FEATURED_LABEL' => 'En vedette?',
+    'TEXT_MANUFACTURER_IS_FEATURED' => 'En vedette',
+];
+
+return $define;
