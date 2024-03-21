741cb07953629c5d704acc93a39cf53d624ede5c
andre <andre.lasfargues@iut-rodez.fr>
2024-03-21 12:50:37 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.mail.php
@@ -0,0 +1,19 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Envoyer un e-mail aux Clients',
+    'TEXT_SUBJECT' => 'Sujet :',
+    'TEXT_FROM' => 'De :',
+    'TEXT_MESSAGE' => 'Message <br />au format Text-Only :',
+    'TEXT_MESSAGE_HTML' => 'Message<br />au format Rich Text :',
+    'TEXT_ATTACHMENTS_LIST' => 'Pièces jointes : ',
+    'TEXT_SELECT_ATTACHMENT' => 'Pièces jointes<br />sur serveur : ',
+    'TEXT_SELECT_ATTACHMENT_TO_UPLOAD' => 'Pièces jointes<br />à uploader<br />&amp; joindre : ',
+    'TEXT_ATTACHMENTS_DIR' => 'Répertoire pour uploader : ',
+    'NOTICE_EMAIL_SENT_TO' => 'Note : E-mail envoyé à : %s',
+    'NOTICE_EMAIL_FAILED_SEND' => 'ATTENTION : Échec d\'envoi du mail à certains destinataires : %s',
+    'ERROR_NO_CUSTOMER_SELECTED' => 'ERREUR : Aucun client sélectionné.',
+    'ERROR_NO_SUBJECT' => 'ERREUR : Aucun sujet indiqué.',
+    'ERROR_ATTACHMENTS' => 'ERREUR : Vous ne pouvez pas sélectionner à la fois UPLOADER et AJOUTER des pièces jointes. Un seul choix est possible.',
+];
+
+return $define;
