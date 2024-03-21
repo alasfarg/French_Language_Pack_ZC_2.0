f94aacef00f72f8c630f1241d7b543e7c05d3ccc
andre <andre.lasfargues@iut-rodez.fr>
2024-03-21 12:50:13 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.invoice.php
@@ -0,0 +1,25 @@
+<?php
+$define = [
+    'TABLE_HEADING_COMMENTS' => 'Commentaires',
+    'TABLE_HEADING_CUSTOMER_NOTIFIED' => 'Client notifié',
+    'TABLE_HEADING_DATE_ADDED' => 'Date de création',
+    'TABLE_HEADING_STATUS' => 'État',
+    'TABLE_HEADING_PRODUCTS' => 'Produits',
+    'TABLE_HEADING_TAX' => 'Taxe',
+    'TABLE_HEADING_TOTAL' => 'Total',
+    'TABLE_HEADING_PRICE_EXCLUDING_TAX' => 'Prix (HT)',
+    'TABLE_HEADING_PRICE_INCLUDING_TAX' => 'Prix (TTC)',
+    'TABLE_HEADING_TOTAL_EXCLUDING_TAX' => 'Total (HT)',
+    'TABLE_HEADING_TOTAL_INCLUDING_TAX' => 'Total (TTC)',
+    'TABLE_HEADING_PRICE' => 'Prix',
+    'ENTRY_CUSTOMER' => 'CLIENT :',
+    'ENTRY_SOLD_TO' => 'VENDU À :',
+    'ENTRY_SHIP_TO' => 'LIVRÉ À :',
+    'ENTRY_PAYMENT_METHOD' => 'Méthode de paiement :',
+    'ENTRY_SHIPPING' => 'Port :',
+    'ENTRY_DATE_PURCHASED' => 'Date de commande :',
+    'ENTRY_ORDER_ID' => 'Facture n° ',
+    'TEXT_INFO_ATTRIBUTE_FREE' => '&nbsp;-&nbsp;GRATUIT',
+];
+
+return $define;
---
+++ admin/includes/languages/french/lang.languages.php
@@ -0,0 +1,22 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Langues',
+    'TABLE_HEADING_LANGUAGE_NAME' => 'Langue',
+    'TABLE_HEADING_LANGUAGE_CODE' => 'Code',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TEXT_INFO_EDIT_INTRO' => 'Veuillez effectuer les changements nécessaires',
+    'TEXT_INFO_LANGUAGE_NAME' => 'Nom :',
+    'TEXT_INFO_LANGUAGE_CODE' => 'Code :',
+    'TEXT_INFO_LANGUAGE_IMAGE' => 'Image :',
+    'TEXT_INFO_LANGUAGE_DIRECTORY' => 'Répertoire :',
+    'TEXT_INFO_LANGUAGE_SORT_ORDER' => 'Classement :',
+    'TEXT_INFO_INSERT_INTRO' => 'Veuillez entrer la nouvelle langue et effectuer les changements requis',
+    'TEXT_INFO_DELETE_INTRO' => 'Êtes-vous certain de vouloir supprimer cette langue ?',
+    'TEXT_INFO_HEADING_NEW_LANGUAGE' => 'Nouvelle langue',
+    'TEXT_INFO_HEADING_EDIT_LANGUAGE' => 'Éditer la langue',
+    'TEXT_INFO_HEADING_DELETE_LANGUAGE' => 'Supprimer la langue',
+    'ERROR_REMOVE_DEFAULT_LANGUAGE' => 'ERREUR : La langue par défaut ne peut être supprimée. Veuillez sélectionner une autre langue avant de réessayer.',
+    'ERROR_DUPLICATE_LANGUAGE_CODE' => 'ERREUR : Une langue associée à ce code existe déjà.',
+];
+
+return $define;
