f0e68245a27de12e9ff75073a130d10c615a1b8f
andre <andre.lasfargues@iut-rodez.fr>
2024-03-21 12:48:59 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.group_pricing.php
@@ -0,0 +1,25 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Groupes de Prix',
+    'TABLE_HEADING_GROUP_ID' => 'ID',
+    'TABLE_HEADING_GROUP_NAME' => 'Nom du Groupe',
+    'TABLE_HEADING_GROUP_AMOUNT' => '% de Réduction',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TEXT_HEADING_NEW_PRICING_GROUP' => 'Nouveau groupe de prix',
+    'TEXT_HEADING_EDIT_PRICING_GROUP' => 'Éditer le groupe de prix',
+    'TEXT_HEADING_DELETE_PRICING_GROUP' => 'Supprimer le groupe de prix',
+    'TEXT_NEW_INTRO' => 'Veuillez entrer les informations pour ce nouveau groupe',
+    'TEXT_EDIT_INTRO' => 'Veuillez effectuer les changements nécessaires.',
+    'TEXT_DELETE_INTRO' => 'Êtes-vous certain(e) de vouloir supprimer ce groupe ?',
+    'TEXT_DELETE_PRICING_GROUP' => 'Supprimer le groupe de prix',
+    'TEXT_DELETE_WARNING_GROUP_MEMBERS' => '<b>ATTENTION :</b> Il y a %s clients encore liés à cette catégorie !',
+    'TEXT_GROUP_PRICING_NAME' => 'Nom du groupe : ',
+    'TEXT_GROUP_PRICING_AMOUNT' => 'Pourcentage de réduction : ',
+    'TEXT_DATE_ADDED' => 'Date de création :',
+    'TEXT_LAST_MODIFIED' => 'Dernière modification :',
+    'TEXT_CUSTOMERS' => 'Clients dans le groupe  :',
+    'ERROR_GROUP_PRICING_CUSTOMERS_EXIST' => 'ERREUR : Des clients existent dans ce groupe. Veuillez confirmer que vous souhaitez enlever tous les membres du groupe et les supprimer.',
+    'ERROR_MODULE_NOT_CONFIGURED' => 'NOTE : Vous avez défini des groupes de prix, mais vous n\'avez pas activé le module total-commande de groupes de prix.<br />Veuillez aller dans Admin-&gt;Modules-&gt;Order Total-&gt;Membership Discount (ot_group_pricing) et installez/configurez le module.',
+];
+
+return $define;
