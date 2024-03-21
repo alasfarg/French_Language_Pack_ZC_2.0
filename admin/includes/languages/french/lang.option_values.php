e18585f3e31fa932d32b85a5453b7615ea825845
andre <andre.lasfargues@iut-rodez.fr>
2024-03-21 17:59:56 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.option_values.php
@@ -0,0 +1,17 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Classement par défaut des valeurs d\'options',
+    'TEXT_UPDATE_OPTION_VALUES' => 'Mise à jour du classement des valeurs d\'options',
+    'TEXT_SELECT_OPTION' => 'Sélectionner un nom d\'option :',
+    'TEXT_EDIT_OPTION_NAME' => 'Éditer le nom de l\'option : ',
+    'TEXT_NO_OPTION_VALUE' => 'Aucune valeur d\'option définie pour <br />le nom d\'option : ',
+    'TEXT_UPDATE_SUBMIT' => 'Mettre à jour le classement',
+    'TEXT_UPDATE_SORT_ORDERS_OPTIONS_PRODUCTS' => '<strong>Pour un produit :</strong> ',
+    'TEXT_UPDATE_SORT_ORDERS_OPTIONS_CATEGORIES' => '<strong>Pour une catégorie :</strong> ',
+    'SUCCESS_PRODUCT_UPDATE_SORT' => 'Mise à jour réussie du classement des attributs pour ID# ',
+    'SUCCESS_PRODUCT_UPDATE_SORT_ALL' => 'Mise à jour réussie du classement des attributs pour TOUS les Produits ',
+    'SUCCESS_OPTION_VALUES_SORT_ORDER' => 'Mise à jour réussie du classement des valeurs d\'option : ',
+    'SUCCESS_CATEGORIES_UPDATE_SORT' => 'Mise à jour réussie du classement des attributs pour la catégorie ID# ',
+];
+
+return $define;
