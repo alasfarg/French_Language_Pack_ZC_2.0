e3dd3c730e54e472ffb626a040b723fbcbf8a35b
andre <andre.lasfargues@iut-rodez.fr>
2024-03-21 12:49:45 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.gv_sent.php
@@ -0,0 +1,21 @@
+<?php
+$define = [
+    'HEADING_TITLE' => TEXT_GV_NAMES.' Envoyé',
+    'TABLE_HEADING_SENDERS_NAME' => 'Nom Expéditeur',
+    'TABLE_HEADING_VOUCHER_VALUE' => 'Valeur du '.TEXT_GV_NAME,
+    'TABLE_HEADING_VOUCHER_CODE' => TEXT_GV_REDEEM,
+    'TABLE_HEADING_DATE_SENT' => 'Date envoyé',
+    'TEXT_HEADING_DATE_REDEEMED' => 'Date remboursé',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TEXT_INFO_SENDERS_ID' => 'ID Expéditeur ',
+    'TEXT_INFO_AMOUNT_SENT' => 'Montant envoyé :',
+    'TEXT_INFO_DATE_SENT' => 'Date d\'envoi :',
+    'TEXT_INFO_VOUCHER_CODE' => TEXT_GV_REDEEM.' :',
+    'TEXT_INFO_EMAIL_ADDRESS' => 'Adresse e-mail :',
+    'TEXT_INFO_DATE_REDEEMED' => 'Date de remboursement :',
+    'TEXT_INFO_IP_ADDRESS' => 'Adresse IP :',
+    'TEXT_INFO_CUSTOMERS_ID' => 'ID Client :',
+    'TEXT_INFO_NOT_REDEEMED' => 'Non remboursé',
+];
+
+return $define;
