aae9e6eda792e5716688d3637301d85d1017f839
andre <andre.lasfargues@iut-rodez.fr>
2024-03-21 12:46:39 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.downloads_manager.php
@@ -0,0 +1,26 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Downloads/Téléchargements',
+    'TABLE_HEADING_ATTRIBUTES_ID' => 'ID# Attr',
+    'TABLE_HEADING_PRODUCTS_ID' => 'ID# Prod',
+    'TABLE_HEADING_PRODUCT' => 'Nom du produit',
+    'TABLE_HEADING_OPT_NAME' => 'Nom d\'option',
+    'TABLE_HEADING_OPT_VALUE' => 'Valeur d\'option',
+    'TABLE_TEXT_FILENAME' => 'Fichier',
+    'TABLE_TEXT_MAX_DAYS' => 'Jours',
+    'TABLE_TEXT_MAX_COUNT' => 'Compteur',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TABLE_HEADING_OPT_PRICE' => 'Prix',
+    'TABLE_HEADING_OPT_PRICE_PREFIX' => 'Préfixe',
+    'TEXT_PRODUCTS_NAME' => 'Produit : ',
+    'TEXT_INFO_HEADING_EDIT_PRODUCTS_DOWNLOAD' => 'MODIFICATION DES INFORMATIONS DE TÉLÉCHARGEMENT',
+    'TEXT_INFO_EDIT_INTRO' => 'Modifier les informations du download :',
+    'TEXT_DELETE_INTRO' => 'Le fichier suivant sera supprimé de la base de données. Il ne sera pas supprimé du serveur :',
+    'TEXT_INFO_FILENAME' => 'Fichier : ',
+    'TEXT_INFO_MAX_DAYS' => 'Max. jours : ',
+    'TEXT_INFO_MAX_COUNT' => 'Max. téléchargements : ',
+    'TEXT_INFO_FILENAME_MISSING' => '&nbsp;Fichier manquant',
+    'TEXT_INFO_FILENAME_GOOD' => '&nbsp;Fichier valide',
+];
+
+return $define;
