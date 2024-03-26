33200463f1a60249cc7d3b6b5dac273e57b1027f
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:18:32 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.option_name.php
@@ -0,0 +1,15 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Ordre de tri du noms d\'options',
+    'TEXT_EDIT_ALL' => 'Édition de tous les noms d\'options',
+    'TEXT_CURRENT_NAME' => 'Nom en cours',
+    'TEXT_SORT_ORDER' => 'Ordre de tri',
+    'TEXT_OPTION_ID' => 'ID Option ',
+    'TEXT_OPTION_TYPE' => 'Type',
+    'TEXT_OPTION_NAME' => 'Nom d\'option',
+    'TEXT_SELECTED_LANGUAGE' => 'Langue sélectionnée: ',
+    'TEXT_UPDATE_SUBMIT' => 'L\'ordre de tri de mettre à jour',
+    'SUCCESS_OPTION_SORT_ORDER' => 'Mise à jour réussie du classement des noms d\'options',
+];
+
+return $define;
---
+++ admin/includes/languages/french/lang.orders_status.php
@@ -0,0 +1,21 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'États de Commande',
+    'TABLE_HEADING_ORDERS_STATUS' => 'États de Commande',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TABLE_HEADING_ORDERS_STATUS_ID' => 'États de Commande ID',
+    'TABLE_HEADING_SORT_ORDER' => 'Trier les commandes par',
+    'TEXT_INFO_SORT_ORDER' => 'Trier par : ',
+    'TEXT_INFO_EDIT_INTRO' => 'Veuillez effectuer les changements nécessaires',
+    'TEXT_INFO_ORDERS_STATUS_NAME' => 'État de commande :',
+    'TEXT_INFO_INSERT_INTRO' => 'Veuillez saisir le nouvel état de commande avec ses données relatives',
+    'TEXT_INFO_DELETE_INTRO' => 'Êtes-vous certain(e) de vouloir supprimer cet état de commande ?',
+    'TEXT_INFO_HEADING_NEW_ORDERS_STATUS' => 'Nouvel état de commande',
+    'TEXT_INFO_HEADING_EDIT_ORDERS_STATUS' => 'Édition de l\'état de commande',
+    'TEXT_INFO_HEADING_DELETE_ORDERS_STATUS' => 'Effacement de l\'état de commande',
+    'ERROR_REMOVE_DEFAULT_ORDER_STATUS' => 'ERREUR : L\'état de commande par défaut ne peut être supprimé. Veuillez définir un autre état de commande par défaut et réessayer.',
+    'ERROR_STATUS_USED_IN_ORDERS' => 'ERREUR : Cet état de commande est actuellement utilisé par des commandes.',
+    'ERROR_STATUS_USED_IN_HISTORY' => 'ERREUR : Cet état de commande est actuellement utilisé dans l\'historique des états de commande.',
+];
+
+return $define;
