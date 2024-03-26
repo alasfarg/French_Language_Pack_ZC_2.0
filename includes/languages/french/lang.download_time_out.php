edcc2d426ecc2e7e691cbfb1c47a633e3484a735
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 13:39:37 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.download_time_out.php
@@ -0,0 +1,10 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Votre téléchargement ...',
+    'HEADING_TITLE' => 'Votre téléchargement ...',
+    'TEXT_INFORMATION' => 'Nous sommes désolés mais votre téléchargement a expiré.<br /><br />
+  Si vous aviez d\'autres téléchargements et que vous voulez les récupérer,
+  veuillez vous rendre sur la page <a href="'.zen_href_link(FILENAME_ACCOUNT,'','SSL',
+];
+
+return $define;
---
+++ includes/languages/french/lang.gv_faq.php
@@ -0,0 +1,13 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'FAQ '.TEXT_GV_NAME,
+    'HEADING_TITLE' => 'FAQ '.TEXT_GV_NAME,
+    'TEXT_INFORMATION' => '<a name="Top"></a>
+  <a href="'.zen_href_link(FILENAME_GV_FAQ,'faq_item=1','NONSSL',
+    'SUB_HEADING_TITLE' => '',
+    'SUB_HEADING_TEXT' => 'Veuillez choisir une des questions ci-dessus.',
+    'TEXT_GV_REDEEM_INFO' => 'Veuillez entrer le code remboursement de votre '.TEXT_GV_NAME.' : ',
+    'TEXT_GV_REDEEM_ID' => 'Code de remboursement: ',
+];
+
+return $define;
