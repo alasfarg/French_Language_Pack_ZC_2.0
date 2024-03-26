c7673c1f31b915faf5783ed2c6c267fb38cb563e
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 18:56:15 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.site_map.php
@@ -0,0 +1,19 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Plan du Site',
+    'HEADING_TITLE' => 'Plan du Site',
+    'TEXT_INFORMATION' => '',
+    'PAGE_ACCOUNT' => 'Mon Compte',
+    'PAGE_ACCOUNT_EDIT' => 'Informations du compte',
+    'PAGE_ADDRESS_BOOK' => 'Carnet d\'adresses',
+    'PAGE_ACCOUNT_HISTORY' => 'Historique de commande',
+    'PAGE_ACCOUNT_NOTIFICATIONS' => 'Abonnement au bulletin',
+    'PAGE_SHOPPING_CART' => 'Panier',
+    'PAGE_CHECKOUT_SHIPPING' => 'Encaissement',
+    'PAGE_ADVANCED_SEARCH' => 'Recherche avancée',
+    'PAGE_PRODUCTS_NEW' => 'Nouveautés',
+    'PAGE_SPECIALS' => 'Promotions',
+    'PAGE_REVIEWS' => 'Avis',
+];
+
+return $define;
