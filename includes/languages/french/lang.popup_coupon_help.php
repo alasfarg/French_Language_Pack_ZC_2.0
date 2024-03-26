b1da28d81d861ecc164809b939e32605ffe197ac
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 18:53:26 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.popup_coupon_help.php
@@ -0,0 +1,24 @@
+<?php
+$define = [
+    'TEXT_CLOSE_WINDOW' => 'Fermer la fenêtre [x]',
+    'TEXT_COUPON_HELP_HEADER' => '<strong>Le code d\'utilisation du coupon de réduction que vous avez entré est pour</strong> ',
+    'TEXT_COUPON_HELP_NAME' => '<br /><br />Nom du coupon : %s',
+    'TEXT_COUPON_HELP_FIXED' => '<br /><br />Ce coupon vous donne droit à %s de remise sur votre commande',
+    'TEXT_COUPON_HELP_MINORDER' => '<br /><br />Vous devez dépenser %s pour utiliser ce coupon',
+    'TEXT_COUPON_HELP_FREESHIP' => '<br /><br />Ce coupon vous donne droit à une livraison gratuite de votre commande',
+    'TEXT_COUPON_HELP_DESC' => '<br /><br />Description du coupon : %s',
+    'TEXT_COUPON_HELP_DATE' => '<br /><br />Ce coupon est valable du %s au %s',
+    'TEXT_COUPON_HELP_RESTRICT' => '<br /><br />Restriction à un produit/une catégorie',
+    'TEXT_COUPON_HELP_CATEGORIES' => 'Catégorie',
+    'TEXT_COUPON_HELP_PRODUCTS' => 'Produit',
+    'TEXT_ALLOWED' => ' (Autorisé)',
+    'TEXT_DENIED' => ' (Interdit)',
+    'TEXT_NO_CAT_TOP_ONLY_DENY' => '<p>Ce coupon a des restrictions de produit spécifiques.</p>',
+    'TEXT_NO_CAT_RESTRICTIONS' => '<p>Ce bon est valable pour toutes les catégories.</p>',
+    'TEXT_NO_PROD_RESTRICTIONS' => '<p>Ce bon est valable pour tous les produits.</p>',
+    'TEXT_NO_PROD_SALES' => '<p>Ce coupon n\'est pas valide pour les produits en promotion.</p>',
+    'TEXT_COUPON_GV_RESTRICTION' => 'Les coupons de réduction ne peuvent être appliqué sur l\'achat de '.TEXT_GV_NAMES,
+    'TEXT_COUPON_GV_RESTRICTION_ZONES' => 'Des restrictions selon l\'adresse de facturation s\'appliquent.',
+];
+
+return $define;
