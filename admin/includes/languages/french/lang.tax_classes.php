39b859345d1d0d30dcc3b32544a76bf80438bea2
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:23:28 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.tax_classes.php
@@ -0,0 +1,21 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Classes de Taxes',
+    'TABLE_HEADING_TAX_CLASS_ID' => 'ID Classe',
+    'TABLE_HEADING_TAX_CLASSES' => 'Classe de Taxes',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TEXT_INFO_EDIT_INTRO' => 'Veuillez effectuer les changements nécessaires',
+    'TEXT_INFO_CLASS_TITLE' => 'Intitulé de la classe de taxes :',
+    'TEXT_INFO_CLASS_DESCRIPTION' => 'Description :',
+    'TEXT_INFO_DATE_ADDED' => 'Date de création :',
+    'TEXT_INFO_LAST_MODIFIED' => 'Dernière modification :',
+    'TEXT_INFO_INSERT_INTRO' => 'Veuillez saisir la nouvelle classe de taxes avec ses données relatives',
+    'TEXT_INFO_DELETE_INTRO' => 'Êtes-vous certain de vouloir supprimer cette classe de taxes ?',
+    'TEXT_INFO_HEADING_NEW_TAX_CLASS' => 'Nouvelle classe de taxes',
+    'TEXT_INFO_HEADING_EDIT_TAX_CLASS' => 'Éditer la classe de taxes',
+    'TEXT_INFO_HEADING_DELETE_TAX_CLASS' => 'Supprimer la classe de taxes',
+    'ERROR_TAX_RATE_EXISTS_FOR_CLASS' => 'ERREUR : Impossible de supprimer cette classe de taxes -- Des taux de taxes sont liés à cette classe de taxes.',
+    'ERROR_TAX_RATE_EXISTS_FOR_PRODUCTS' => 'ERREUR : Impossible de supprimer cette classe de taxes -- Il y a %s produits qui utilisent cette classe de taxes.',
+];
+
+return $define;
---
+++ admin/includes/languages/french/lang.tax_rates.php
@@ -0,0 +1,26 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Taux de Taxe',
+    'TABLE_HEADING_TAX_RATE_PRIORITY' => 'Priorité',
+    'TABLE_HEADING_TAX_CLASS_TITLE' => 'Classe de Taxes',
+    'TABLE_HEADING_COUNTRIES_NAME' => 'Pays',
+    'TABLE_HEADING_ZONE' => 'Zone de Taxes',
+    'TABLE_HEADING_TAX_RATE' => 'Taux de Taxe',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TEXT_INFO_EDIT_INTRO' => 'Veuillez effectuer les changements nécessaires',
+    'TEXT_INFO_DATE_ADDED' => 'Date de création :',
+    'TEXT_INFO_LAST_MODIFIED' => 'Dernière modification :',
+    'TEXT_INFO_CLASS_TITLE' => 'Intitulé de la classe de taxes :',
+    'TEXT_INFO_COUNTRY_NAME' => 'Pays :',
+    'TEXT_INFO_ZONE_NAME' => 'Zone de taxes :',
+    'TEXT_INFO_TAX_RATE' => 'Taux de taxe (%) :',
+    'TEXT_INFO_TAX_RATE_PRIORITY' => 'Les taux de taxe de même priorité sont additionnés, les autres sont combinés.<br><br>Priorité :',
+    'TEXT_INFO_RATE_DESCRIPTION' => 'Description :',
+    'TEXT_INFO_INSERT_INTRO' => 'Veuillez saisir le nouveau taux de taxe et ses données relatives',
+    'TEXT_INFO_DELETE_INTRO' => 'Êtes-vous certain de vouloir effacer ce taux de taxe ?',
+    'TEXT_INFO_HEADING_NEW_TAX_RATE' => 'Nouveau taux de taxe',
+    'TEXT_INFO_HEADING_EDIT_TAX_RATE' => 'Éditer le taux de taxe',
+    'TEXT_INFO_HEADING_DELETE_TAX_RATE' => 'Supprimer le taux de taxe',
+];
+
+return $define;
