7a4167d60f7ae535108ebde317e9e7fec000a8a7
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:26:05 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/extra-definitions/lang.product_music.php
@@ -0,0 +1,13 @@
+<?php
+$define = [
+    'BOX_CATALOG_RECORD_ARTISTS' => 'Artistes',
+    'BOX_CATALOG_RECORD_COMPANY' => 'Maisons de Disques',
+    'BOX_CATALOG_MUSIC_GENRE' => 'Genres de Musique',
+    'BOX_CATALOG_MEDIA_MANAGER' => 'Gestion des Médias',
+    'BOX_CATALOG_MEDIA_TYPES' => 'Types de Médias',
+    'TEXT_PRODUCTS_RECORD_ARTIST' => 'Artiste :',
+    'TEXT_PRODUCTS_RECORD_COMPANY' => 'Maison de Disques :',
+    'TEXT_PRODUCTS_MUSIC_GENRE' => 'Genres de Musique :',
+];
+
+return $define;
