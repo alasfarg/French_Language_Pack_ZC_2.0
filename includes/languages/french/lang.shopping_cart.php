ccd187bc605eeb9a06e2725894b40e2719eacafe
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 18:55:57 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.shopping_cart.php
@@ -0,0 +1,17 @@
+<?php
+$define = [
+    'TEXT_INFORMATION' => 'Vous pouvez procéder à votre achat en cliquant sur le bouton Commander ci-dessous. L\'expédition et les taxes et les réductions seront traitées sur les pages suivantes. ',
+    'NAVBAR_TITLE' => 'Panier',
+    'HEADING_TITLE' => 'Contenu de votre panier',
+    'TABLE_HEADING_REMOVE' => 'Supprimer',
+    'TABLE_HEADING_PRICE' => 'Unitaire',
+    'TEXT_CART_EMPTY' => 'Votre panier est vide.',
+    'SUB_TITLE_SUB_TOTAL' => 'Sous-total:',
+    'TEXT_TOTAL_ITEMS' => 'Article(s): ',
+    'TEXT_TOTAL_WEIGHT' => '&nbsp;&nbsp;Poids: ',
+    'TEXT_TOTAL_AMOUNT' => '&nbsp;&nbsp;Montant: ',
+    'TEXT_CART_HELP' => '<a href="javascript:session_win();">[Aide (?)]</a>',
+    'TEXT_VISITORS_CART' => TEXT_CART_HELP,
+];
+
+return $define;
