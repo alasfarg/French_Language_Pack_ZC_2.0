65d4c2e96bbd3ed1b8e80e283f5ed146d5bf7123
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:22:52 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.stats_products_lowstock.php
@@ -0,0 +1,9 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Produits en Stock',
+    'TABLE_HEADING_NUMBER' => 'ID#',
+    'TABLE_HEADING_PRODUCTS' => 'Produits',
+    'TABLE_HEADING_VIEWED' => 'Quantité',
+];
+
+return $define;
---
+++ admin/includes/languages/french/lang.stats_products_purchased.php
@@ -0,0 +1,15 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Produits les plus achetés',
+    'TABLE_HEADING_NUMBER' => 'ID#',
+    'TABLE_HEADING_PRODUCTS' => 'Produits',
+    'TABLE_HEADING_PURCHASED' => 'Achetés',
+    'TABLE_HEADING_CUSTOMERS_ID' => 'Client<br />ID#',
+    'TABLE_HEADING_ORDERS_ID' => 'Commande<br />ID#',
+    'TABLE_HEADING_ORDERS_DATE_PURCHASED' => 'Date',
+    'TABLE_HEADING_CUSTOMERS_INFO' => 'Client',
+    'TABLE_HEADING_PRODUCTS_QUANTITY' => 'Qté',
+    'TABLE_HEADING_PRODUCTS_NAME' => 'Produit',
+];
+
+return $define;
