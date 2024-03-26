9c17af6f217dd3ec73f8bc97189731426d4548bf
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:19:45 +0100

    Téléverser les fichiers vers "admin/includes/languages/french"

---
+++ admin/includes/languages/french/lang.paypal.php
@@ -0,0 +1,28 @@
+<?php
+$define = [
+    'TEXT_PAYPAL_IPN_SORT_ORDER_INFO' => 'Afficher la commande: ',
+    'TEXT_SORT_PAYPAL_ID_DESC' => 'Commande PayPal reçue (nouveau - ancien)',
+    'TEXT_SORT_PAYPAL_ID' => 'Commande PayPal reçue (ancien - nouveau)',
+    'TEXT_SORT_ZEN_ORDER_ID_DESC' => 'ID Commande (haut - bas), Commande PayPal reçue',
+    'TEXT_SORT_ZEN_ORDER_ID' => 'ID Commande (bas - haut), Commande PayPal reçue',
+    'TEXT_PAYMENT_AMOUNT_DESC' => 'Montant de la commande (haut - bas)',
+    'TEXT_PAYMENT_AMOUNT' => 'Montant de la commande (bas - haut)',
+    'HEADING_ADMIN_TITLE' => 'Notifications de Paiement Instantané PayPal',
+    'HEADING_PAYMENT_STATUS' => 'Statut PayPal',
+    'TEXT_ALL_IPNS' => 'Tout',
+    'TABLE_HEADING_ORDER_NUMBER' => 'Commande #',
+    'TABLE_HEADING_PAYPAL_ID' => 'PayPal #',
+    'TABLE_HEADING_TXN_TYPE' => 'Type de transaction',
+    'TABLE_HEADING_PAYMENT_STATUS' => 'Statut du paiement',
+    'TABLE_HEADING_PAYMENT_AMOUNT' => 'Montant',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TABLE_HEADING_DATE_ADDED' => 'Date de création',
+    'TABLE_HEADING_NUM_HISTORY_ENTRIES' => 'Nombre d\'entrées dans l\'historique des statuts',
+    'TABLE_HEADING_ENTRY_NUM' => 'Entrée n&deg;',
+    'TABLE_HEADING_TRANS_ID' => 'ID Trans.',
+    'TABLE_HEADING_PENDING_REASON' => 'Raison en attente',
+    'TEXT_INFO_PAYPAL_IPN_HEADING' => 'PayPal IPN',
+    'TEXT_DISPLAY_PAYPAL_IPN_NUMBER_OF_TX' => 'Affiche <b>%d</b> à <b>%d</b> (sur <b>%d</b> Transactions)',
+];
+
+return $define;
