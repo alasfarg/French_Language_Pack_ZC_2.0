ab18488f806d351624cbb5c1d592ac761c713426
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:05:24 +0100

    ajout fichiers

---
+++ includes/languages/french/responsive_classic/lang.icon_names.php
@@ -0,0 +1,17 @@
+<?php
+$define = [
+    'ICON_IMAGE_ERROR' => 'error.png',
+    'ICON_IMAGE_WARNING' => 'warning.png',
+    'ICON_IMAGE_SUCCESS' => 'success.png',
+    'ICON_IMAGE_TRASH' => 'small_delete.png',
+    'ICON_IMAGE_UPDATE' => 'button_update_cart.png',
+    'ICON_IMAGE_TINYCART' => 'cart.gif',
+    'ICON_ERROR_ALT' => 'Erreur',
+    'ICON_SUCCESS_ALT' => 'Succès',
+    'ICON_WARNING_ALT' => 'Avertissement',
+    'ICON_TRASH_ALT' => 'Supprimer cet article du panier en cliquant sur cette icône.',
+    'ICON_UPDATE_ALT' => 'Modifiez votre quantité en mettant en évidence le nombre dans la case, en corrigeant la quantité et en cliquant sur ce bouton.',
+    'ICON_TINYCART_ALT' => 'Ajoutez ce produit à votre panier en cliquant ici.',
+];
+
+return $define;
---
+++ includes/languages/french/responsive_classic/lang.index.php
@@ -0,0 +1,24 @@
+<?php
+$define = [
+    'TEXT_MAIN' => 'Ceci est la principale instruction de définition de la page en français quand aucun fichier de modèle défini n\'existe. Elle se trouve dans : <strong>/includes/languages/french/index.php</strong>',
+    'TEXT_GREETING_GUEST' => 'Bienvenue, veuillez profiter de notre vitrine en ligne.',
+    'TEXT_GREETING_PERSONAL' => 'Bonjour <span class="greetUser">%s</span>! Souhaitez-vous voir nos <a href="%s">derniers ajouts</a>?',
+    'TEXT_INFORMATION' => 'Définissez votre copie principale de la page Index ici.',
+    'HEADING_TITLE' => 'Bienvenue cher client/chère cliente dans notre boutique en ligne.',
+    'TABLE_HEADING_IMAGE' => '',
+    'TABLE_HEADING_PRODUCTS' => 'Nom du produit',
+    'TABLE_HEADING_MANUFACTURER' => 'Fabricant',
+    'TABLE_HEADING_PRICE' => 'Prix',
+    'TABLE_HEADING_WEIGHT' => 'Poids',
+    'TABLE_HEADING_BUY_NOW' => 'Acheter maintenant',
+    'TEXT_NO_PRODUCTS' => 'Il n\'y a aucun produit à lister dans cette catégorie.',
+    'TEXT_NO_PRODUCTS2' => 'Aucun produit n\'est disponible chez ce fabricant.',
+    'TEXT_NUMBER_OF_PRODUCTS' => 'Nombre de produits : ',
+    'TEXT_SHOW' => 'Filtrer les résultats par :',
+    'TEXT_BUY' => 'Acheter 1 \'',
+    'TEXT_NOW' => '\' maintenant',
+    'TEXT_ALL_CATEGORIES' => 'Toutes les catégories',
+    'TEXT_ALL_MANUFACTURERS' => 'Tous les fabricants',
+];
+
+return $define;
