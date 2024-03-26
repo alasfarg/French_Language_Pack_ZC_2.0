6510a1894ce568fe5913acb96aa3aa57ba2804c9
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 13:40:14 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.featured_products.php
@@ -0,0 +1,13 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Produits phares',
+    'HEADING_TITLE' => 'Produits Phares',
+    'TEXT_DATE_ADDED' => 'Date de création: ',
+    'TEXT_MANUFACTURER' => 'Fabricant: ',
+    'TEXT_PRICE' => 'Prix: ',
+    'TEXT_PRODUCTS_WEIGHT' => 'Poids: ',
+    'TEXT_PRODUCTS_QUANTITY' => 'En stock: ',
+    'TEXT_OUT_OF_STOCK' => 'Épuisé',
+];
+
+return $define;
