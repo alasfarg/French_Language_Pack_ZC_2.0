5e4dcdba0c1b11981d54c91da07b4e76b02815a8
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 14:48:48 +0100

    correct lignes from english

--- includes/languages/french/lang.gv_faq.php
+++ includes/languages/french/lang.gv_faq.php
@@ -2,10 +2,57 @@
 $define = [
     'NAVBAR_TITLE' => 'FAQ '.TEXT_GV_NAME,
     'HEADING_TITLE' => 'FAQ '.TEXT_GV_NAME,
-    'TEXT_INFORMATION' => '<a name="Top"></a>
-  <a href="'.zen_href_link(FILENAME_GV_FAQ,'faq_item=1','NONSSL',
-    'SUB_HEADING_TITLE' => '',
-    'SUB_HEADING_TEXT' => 'Veuillez choisir une des questions ci-dessus.',
+    'TEXT_INFORMATION' => '<a id="Top"></a>
+  <a href="' . zen_href_link(FILENAME_GV_FAQ, 'faq_item=1') . '">Achats ' . TEXT_GV_NAMES . '</a><br>
+  <a href="' . zen_href_link(FILENAME_GV_FAQ, 'faq_item=2') . '">Comment envoyer ' . TEXT_GV_NAMES . '</a><br>
+  <a href="' . zen_href_link(FILENAME_GV_FAQ, 'faq_item=3') . '">Acheter avec ' . TEXT_GV_NAMES . '</a><br>
+  <a href="' . zen_href_link(FILENAME_GV_FAQ, 'faq_item=4') . '">Rachat ' . TEXT_GV_NAMES . '</a><br>
+  <a href="' . zen_href_link(FILENAME_GV_FAQ, 'faq_item=5') . '">Lorsque des problèmes surviennent</a><br>',
+    'SUB_HEADING_TITLE_1' => 'Achat ' . TEXT_GV_NAMES,
+    'SUB_HEADING_TEXT_1' => TEXT_GV_NAMES . ' sont achetés comme n\'importe quel autre article dans notre magasin. Vous pouvez
+  les payer en utilisant le(s) mode(s) de paiement standard du magasin.
+  Une fois acheté, la valeur du ' . TEXT_GV_NAME . 'sera ajouté à votre compte personnel
+   ' . TEXT_GV_NAME . ' Solde. Si vous avez des fonds dans votre ' . TEXT_GV_NAME . ' Solde, vous
+  remarquerez que le montant apparaît désormais sur la page Mon compte et fournit également un
+  lien vers une page où vous pouvez envoyer le ' . TEXT_GV_NAME . ' à quelqu\'un par e-mail.',
+    'SUB_HEADING_TITLE_2' => 'Comment envoyer ' . TEXT_GV_NAMES,
+    'SUB_HEADING_TEXT_2' => 'Vous pouvez envoyer un ' . TEXT_GV_NAME . ' sur la page Mon compte.
+  Lorsque vous envoyez un ' . TEXT_GV_NAME . ' vous devez préciser les éléments suivants :
+  Le nom de la personne à qui vous envoyez le ' . TEXT_GV_NAME . ';
+  L\'adresse email de la personne à qui vous envoyez le ' . TEXT_GV_NAME . ';
+  Le montant que vous souhaitez envoyer (notez que vous n\'êtes pas obligé d\'envoyer le montant total qui
+  est dans votre ' . TEXT_GV_NAME . ' Solde.,
+  Un court message facultatif qui apparaîtra dans l\'e-mail ;
+  Veuillez vous assurer que vous avez saisi correctement toutes les informations, même si
+  vous aurez la possibilité de le modifier autant de fois que vous le souhaitez avant que
+  l\'e-mail soit effectivement envoyé.',
+    'SUB_HEADING_TITLE_3' => 'Acheter avec ' . TEXT_GV_NAMES,
+    'SUB_HEADING_TEXT_3' => 'Si vous avez des fonds dans votre ' . TEXT_GV_NAME . ' Solde, vous pouvez utiliser ces fonds pour
+  achetez d\'autres articles dans notre magasin. Au moment du paiement, une boîte supplémentaire apparaîtra montrant votre solde. Entrez le montant à appliquer sur les fonds dans votre ' . TEXT_GV_NAME . ' Balance.
+  Attention : vous devrez quand même sélectionner un autre mode de paiement s\'il y a
+  n\'est pas suffisant dans votre ' . TEXT_GV_NAME . ' Solde pour couvrir le coût de votre achat.
+  Si vous avez plus de fonds dans votre ' . TEXT_GV_NAME . ' Solde que le coût total de
+  votre achat, le solde restant sera laissé dans votre ' . TEXT_GV_NAME . ' Solde pour un
+  usage futur.',
+    'SUB_HEADING_TITLE_4' => 'Rachat ' . TEXT_GV_NAMES,
+    'SUB_HEADING_TEXT_4' => 'Si vous recevez un ' . TEXT_GV_NAME . ' par e-mail, il contiendra les détails de la personne qui vou l\'a envoyé
+  ' . TEXT_GV_NAME . ', accompagné d\'un court message de sa part. Le courriel
+  contiendra également le ' . TEXT_GV_NAME . '' . TEXT_GV_REDEEM . '. C\'est probablement une bonne idée d\'imprimer
+  cet e-mail pour référence future. Vous pouvez maintenant racheter le ' . TEXT_GV_NAME . '  de deux manières :<br><br>
+  1. En cliquant à cet effet sur le lien contenu dans l\'email.
+  Cela vous mènera à l\'option "Échanger" du magasin ' . TEXT_GV_NAME . ' page. Il vous sera alors demandé
+  de créer un compte avant que le ' . TEXT_GV_NAME . ' soit validé et placé dans votre
+   ' . TEXT_GV_NAME . ' Solde. Vous pouvez ensuite utiliser le montant pour acheter n\'importe quel article dans notre magasin.<br><br>
+  2. Pendant le processus de paiement, sur la même page que celle sur laquelle vous sélectionnez un mode de paiement,
+il y aura un champ pour saisir un ' . TEXT_GV_REDEEM . '. Entrez le ' . TEXT_GV_REDEEM . ' ici et
+cliquez sur le bouton Utiliser. Le code sera
+ validé et le montant ajouté à votre ' . TEXT_GV_NAME . ' Balance. Vous pouvez ensuite utiliser le montant pour acheter n\'importe quel article dans notre magasin.',
+    'SUB_HEADING_TITLE_5' => 'Quand des problèmes surviennent.',
+    'SUB_HEADING_TEXT_5' => 'Pour toute question concernant le ' . TEXT_GV_NAME . 'Système, veuillez contacter le magasin
+  en utilisant notre page <a href="' . zen_href_link(FILENAME_CONTACT_US) . '">Contactez-nous</a>. S\'il vous plaît, assurez-vous de donner
+ autant d\'informations que possible concernant le problème afin que nous puissions résoudre pleinement le problème.',
+    'SUB_HEADING_TITLE_0' => '',
+    'SUB_HEADING_TEXT_0' => 'Please choose from one of the questions above.',
     'TEXT_GV_REDEEM_INFO' => 'Veuillez entrer le code remboursement de votre '.TEXT_GV_NAME.' : ',
     'TEXT_GV_REDEEM_ID' => 'Code de remboursement: ',
 ];
