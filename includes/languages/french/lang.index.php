88e0834d3c38e28cfa800852aa45c68ab5fce815
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 13:41:49 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.index.php
@@ -0,0 +1,24 @@
+<?php
+$define = [
+    'TEXT_MAIN' => 'C\'est le define principal pour la page française lorsqu\'aucun fichier défini par template n\'existe. Il se trouve dans: <strong>/includes/languages/french/index.php</strong>',
+    'TEXT_GREETING_GUEST' => 'Bienvenue, réjouissez-vous devant notre vitrine en ligne.',
+    'TEXT_GREETING_PERSONAL' => 'Bonjour <span class="greetUser">%s</span>! Voulez-vous voir nos dernières <a href="%s">nouveautés</a> ?',
+    'TEXT_INFORMATION' => 'Définissez ici la copie de votre page d\'index principale.',
+    'HEADING_TITLE' => 'Bienvenue',
+    'TABLE_HEADING_IMAGE' => 'Image Produit',
+    'TABLE_HEADING_PRODUCTS' => 'Nom du produit',
+    'TABLE_HEADING_MANUFACTURER' => 'Fabricant',
+    'TABLE_HEADING_PRICE' => 'Prix',
+    'TABLE_HEADING_WEIGHT' => 'Poids',
+    'TABLE_HEADING_BUY_NOW' => 'Acheter maintenant',
+    'TEXT_NO_PRODUCTS' => 'Aucun produit n\'est disponible dans cette catégorie.',
+    'TEXT_NO_PRODUCTS2' => 'Aucun produit n\'est disponible chez ce fabricant.',
+    'TEXT_NUMBER_OF_PRODUCTS' => 'Nombre de produits : ',
+    'TEXT_SHOW' => 'Filtrer les résultats par : ',
+    'TEXT_BUY' => 'Acheter 1 \'',
+    'TEXT_NOW' => '\' maintenant',
+    'TEXT_ALL_CATEGORIES' => 'Toutes catégories',
+    'TEXT_ALL_MANUFACTURERS' => 'Tous fabricants',
+];
+
+return $define;
