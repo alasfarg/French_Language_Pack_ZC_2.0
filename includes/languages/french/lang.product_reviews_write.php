25146782fc40ddcf991243038d2c5b1642cde0f6
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 18:55:20 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.product_reviews_write.php
@@ -0,0 +1,17 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Avis',
+    'SUB_TITLE_FROM' => 'Écrit par: ',
+    'SUB_TITLE_REVIEW' => 'Dites nous ce que vous pensez du produit et partagez votre avis avec nos autres clients. Assurez vous de concentrer vos commentaires sur le produit.',
+    'SUB_TITLE_RATING' => 'Choisissez une note pour cet article. 1 étoile est le pire, 5 étoiles est le meilleur.',
+    'TEXT_NO_HTML' => '<strong>NOTE:</strong> Les balises HTML ne sont pas autorisées.',
+    'TEXT_BAD' => 'Médiocre',
+    'TEXT_APPROVAL_REQUIRED' => '<strong>NOTE:</strong> Les avis doivent être approuvés avant qu\'ils soient publiés.',
+    'EMAIL_REVIEW_PENDING_SUBJECT' => 'Avis de produit en attente d\'approbation: %s',
+    'EMAIL_PRODUCT_REVIEW_CONTENT_INTRO' => 'Un avis sur le produit %s a été envoyé et nécessite votre approbation.'."\n\n",
+    'EMAIL_PRODUCT_REVIEW_CONTENT_DETAILS' => 'Détails de l\'avis: %s',
+    'TEXT_REVIEW_SUBMITTED_FOR_REVIEW' => 'Merci, votre message a été soumis pour examen.',
+    'TEXT_REVIEW_SUBMITTED' => 'Merci vous d\'avoir soumis votre avis!',
+];
+
+return $define;
---
+++ includes/languages/french/lang.products_all.php
@@ -0,0 +1,13 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Tous les Produits',
+    'HEADING_TITLE' => 'Tous les Produits',
+    'TEXT_DATE_ADDED' => 'Date de création:',
+    'TEXT_MANUFACTURER' => 'Fabricant:',
+    'TEXT_PRICE' => 'Prix:',
+    'TEXT_PRODUCTS_WEIGHT' => 'Poids: ',
+    'TEXT_PRODUCTS_QUANTITY' => 'En stock: ',
+    'TEXT_OUT_OF_STOCK' => 'Épuisé',
+];
+
+return $define;
