1e16bac4e8a8a39d7ee4710b00ab5206645ce900
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:11:24 +0100

    ajout fichiers

---
+++ includes/languages/french/html_includes/responsive_classic/define_conditions.php
@@ -0,0 +1,5 @@
+<p><strong>Conditions générales - Exemple de texte...</strong></p>
+<p>Ce texte peut être modifié dans l'éditeur des pages 'define' situé dans le menu Outils de l'Administration.</p>
+<p>Pour enlever ce texte, supprimez le avec ce même éditeur de pages.</p>
+<p>Ce fichier est <code>/languages/french/html_includes/resposive_classic/define_conditions.php</code></p>
+<p><strong>NOTE: Sauvegardez toujours les fichiers dans <code>/languages/french/html_includes/your_template</code></strong></p>
