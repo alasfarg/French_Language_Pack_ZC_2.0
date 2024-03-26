d68ea0d01de9ca457861de254dc0de5c17a977db
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:01:45 +0100

    ajout fichiers

---
+++ includes/languages/french/modules/payment/lang.moneyorder.php
@@ -0,0 +1,8 @@
+<?php
+$define = [
+    'MODULE_PAYMENT_MONEYORDER_TEXT_TITLE' => 'Chèque/Virement Bancaire',
+    'MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION' => 'Les clients peuvent envoyer leur paiement par courrier. Leur e-mail de confirmation de commande leur demandera de : <br /><br />Veuillez établir votre chèque ou virement bancaire à l\'ordre de :<br />'.(defined('%%MODULE_PAYMENT_MONEYORDER_PAYTO%%')),
+    'MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER' => "Veuillez établir votre chèque ou virement bancaire à l\'ordre de :"."\n\n".MODULE_PAYMENT_MONEYORDER_PAYTO."\n\nEnvoyez votre paiement à :\n".STORE_NAME_ADDRESS."\n\n".'Votre commande ne sera envoyée qu\'à réception du règlement et de sa validation par notre banque.',
+];
+
+return $define;
