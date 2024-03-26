8e327429510b539ff77b7a6cd2ccd318c3ddf313
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 18:52:07 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.page_not_found.php
@@ -0,0 +1,19 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Page non trouvée',
+    'HEADING_TITLE' => 'Page non trouvée',
+    'TEXT_INFORMATION' => '',
+    'PAGE_ACCOUNT' => 'Mon compte',
+    'PAGE_ACCOUNT_EDIT' => 'Informations du compte',
+    'PAGE_ADDRESS_BOOK' => 'Carnet d\'adresses',
+    'PAGE_ACCOUNT_HISTORY' => 'Historique de commande',
+    'PAGE_ACCOUNT_NOTIFICATIONS' => 'Abonnements au bulletin',
+    'PAGE_SHOPPING_CART' => 'Panier',
+    'PAGE_CHECKOUT_SHIPPING' => 'Encaisser',
+    'PAGE_ADVANCED_SEARCH' => 'Recherche avancée',
+    'PAGE_PRODUCTS_NEW' => 'Nouveaux produits',
+    'PAGE_SPECIALS' => 'Promotions',
+    'PAGE_REVIEWS' => 'Avis',
+];
+
+return $define;
