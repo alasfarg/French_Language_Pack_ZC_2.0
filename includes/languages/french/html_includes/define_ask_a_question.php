8cd419067007236a60465f3bd6825184645cc1be
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:09:08 +0100

    ajout fichiers

---
+++ includes/languages/french/html_includes/define_ask_a_question.php
@@ -0,0 +1,1 @@
+<p>Vous avez une question sur le produit? Nous sommes heureux de vous aider!</p>
---
+++ includes/languages/french/html_includes/define_checkout_success.php
@@ -0,0 +1,3 @@
+<p><strong>Encaissement réussi - Exemple de texte ...</strong></p>
+<p>Quelques mots au sujet du délai de livraison ou de votre politique de traitement des commandes peuvent être mis ici.</p>
+<p>Ce texte peut être modifié dans l'éditeur des pages 'define' situé dans le menu Outils de l'Administration.</p>
