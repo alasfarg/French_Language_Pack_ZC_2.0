8675614682903c4e92ace59692cb7e4c54a9975d
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 14:16:50 +0100

    correct lignes from english

--- includes/languages/french/lang.document_product_info.php
+++ includes/languages/french/lang.document_product_info.php
@@ -1,26 +1,5 @@
 <?php
 $define = [
-    'TEXT_PRODUCT_NOT_FOUND' => 'Désolé, le produit n\'a pas été trouvé.',
-    'TEXT_CURRENT_REVIEWS' => 'Avis en cours: ',
-    'TEXT_MORE_INFORMATION' => 'Pour plus d\'informations, veuillez consulter la <a href="%s" target="_blank">page web</a> du produit.',
-    'TEXT_DATE_ADDED' => 'Ce produit a été ajouté à notre catalogue le %s.',
-    'TEXT_DATE_AVAILABLE' => 'Ce produit sera en stock le %s.',
-    'TEXT_ALSO_PURCHASED_PRODUCTS' => 'Les clients qui ont acheté ce produit ont également acheté ...',
-    'TEXT_PRODUCT_OPTIONS' => 'Veuillez choisir: ',
-    'TEXT_PRODUCT_MANUFACTURER' => 'Fabriqué par: ',
-    'TEXT_PRODUCT_WEIGHT' => 'Poids de livraison: ',
-    'TEXT_PRODUCT_QUANTITY' => ' unités en stock',
-    'PREV_NEXT_PRODUCT' => 'Produit ',
-    'TEXT_ATTRIBUTES_PRICE_WAS' => ' [était : ',
-    'TEXT_ATTRIBUTE_IS_FREE' => ' est à présent : Gratuit]',
-    'TEXT_ONETIME_CHARGE_SYMBOL' => ' *',
-    'TEXT_ONETIME_CHARGE_DESCRIPTION' => ' Des frais uniques peuvent s\'appliquer',
-    'TEXT_ATTRIBUTES_QTY_PRICE_HELP_LINK' => 'Remises disponibles par quantités',
-    'ATTRIBUTES_QTY_PRICE_SYMBOL' => zen_image(DIR_WS_TEMPLATE_ICONS.'icon_status_green.gif',TEXT_ATTRIBUTES_QTY_PRICE_HELP_LINK,10,10,
-    'ATTRIBUTES_PRICE_DELIMITER_PREFIX' => ' ( ',
-    'ATTRIBUTES_PRICE_DELIMITER_SUFFIX' => ' )',
-    'ATTRIBUTES_WEIGHT_DELIMITER_PREFIX' => ' (',
-    'ATTRIBUTES_WEIGHT_DELIMITER_SUFFIX' => ') ',
 ];
 
 return $define;
