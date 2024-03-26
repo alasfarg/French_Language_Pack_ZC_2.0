609707943b22ed18a9c103ce21fc80f0e1ac2afd
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 18:53:57 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.popup_search_help.php
@@ -0,0 +1,8 @@
+<?php
+$define = [
+    'HEADING_SEARCH_HELP' => 'Aide à la recherche',
+    'TEXT_SEARCH_HELP' => 'Les mots-clefs peuvent être séparés par les liaisons AND et/ou OR pour un meilleur contrôle des résultats de recherche.<br /><br />Par exemple, <span style="text-decoration:underline;">Microsoft AND mouse</span> donnera les résultats qui contiennent les deux mots. Par contre, pour <u>mouse OR keyboard</u>, les résultats contiendront l\'un des deux mots ou les deux.<br /><br />Des correspondances exactes de mots peuvent être cherchées en entourant les mots-clefs par des guillemets.<br /><br />Par exemple, <span style="text-decoration:underline">"ordinateur portable"</span> retournera les résultats qui contiennent exactement cette chaine de caractères.<br /><br />Des parenthèses peuvent être utlisé pour faire des recherches encore plus précises.<br /><br />Par exemple, <span style="text-decoration:underline;">Microsoft and (keyboard or mouse or "visual basic")</span>.',
+    'TEXT_CLOSE_WINDOW' => '<span class="pseudolink">Fermer la fenêtre</span> [x]',
+];
+
+return $define;
---
+++ includes/languages/french/lang.popup_shipping_estimator.php
@@ -0,0 +1,9 @@
+<?php
+$define = [
+    'HEADING_SEARCH_HELP' => 'Estimateur des coûts de livraison :',
+    'TEXT_TOTAL_ITEMS' => 'Nbre total d\'articles: ',
+    'TEXT_TOTAL_WEIGHT' => '&nbsp;&nbsp;Poids: ',
+    'TEXT_TOTAL_AMOUNT' => '&nbsp;&nbsp;Montant: ',
+];
+
+return $define;
