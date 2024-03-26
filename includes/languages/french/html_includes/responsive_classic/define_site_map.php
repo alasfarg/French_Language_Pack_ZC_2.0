bd6feafbe202d08dfb5731f8cd255f53e64ac9f5
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:13:19 +0100

    ajout fichiers

---
+++ includes/languages/french/html_includes/responsive_classic/define_site_map.php
@@ -0,0 +1,2 @@
+<p><strong>Pour vous aider à naviguer sur notre site, nous avons fourni la carte suivante.</strong></p>
+<p>Si vous rencontrez des difficultés pour localiser quelque chose sur notre site, veuillez visiter notre page <a href="<?php echo zen_href_link(FILENAME_CONTACT_US, '', 'SSL'); ?>">Nous contacter </a>et faites-nous savoir !</p>
