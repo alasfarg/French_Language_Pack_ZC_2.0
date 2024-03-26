88b6af54ea895dbb5e681d6a4152b3e83cef5bf6
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 14:19:54 +0100

    correct lignes from english

--- includes/languages/french/lang.download_time_out.php
+++ includes/languages/french/lang.download_time_out.php
@@ -4,7 +4,9 @@ $define = [
     'HEADING_TITLE' => 'Votre téléchargement ...',
     'TEXT_INFORMATION' => 'Nous sommes désolés mais votre téléchargement a expiré.<br /><br />
   Si vous aviez d\'autres téléchargements et que vous voulez les récupérer,
-  veuillez vous rendre sur la page <a href="'.zen_href_link(FILENAME_ACCOUNT,'','SSL',
+  veuillez vous rendre sur la page <a href="'.zen_href_link(FILENAME_ACCOUNT) . '">Mon compte</a> pour consulter votre commande.<br><br>
+  Ou si vous pensez qu\'il y a un problème avec votre commande, veuillez <a href="' . zen_href_link(FILENAME_CONTACT_US) . '">Contactez-nous</a> <br><br>
+  Merci!',
 ];
 
 return $define;
