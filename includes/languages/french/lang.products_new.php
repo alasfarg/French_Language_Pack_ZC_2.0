c83ff523ea01adac437a8a943c8d820626e92959
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 18:55:38 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.products_new.php
@@ -0,0 +1,13 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Nouveautés',
+    'HEADING_TITLE' => 'Nouveautés',
+    'TEXT_DATE_ADDED' => 'Date de création: ',
+    'TEXT_MANUFACTURER' => 'Fabricant: ',
+    'TEXT_PRICE' => 'Prix: ',
+    'TEXT_PRODUCTS_WEIGHT' => 'Poids: ',
+    'TEXT_PRODUCTS_QUANTITY' => 'En Stock: ',
+    'TEXT_OUT_OF_STOCK' => 'Hors Stock',
+];
+
+return $define;
---
+++ includes/languages/french/lang.reviews.php
@@ -0,0 +1,8 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Avis',
+    'HEADING_TITLE' => 'Avis',
+    'TEXT_OF_5_STARS' => '',
+];
+
+return $define;
