2a4966915171a0a22dc5008c235bd707ea416d89
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:01:11 +0100

    ajout fichiers

---
+++ includes/languages/french/modules/payment/lang.authorizenet.php
@@ -0,0 +1,20 @@
+<?php
+$define = [
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_ADMIN_TITLE' => 'Authorize.net (SIM)',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_CATALOG_TITLE' => 'Carte de Crédit',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_DESCRIPTION' => '<a rel="noreferrer noopener" target="_blank" href="https://reseller.authorize.net/application?resellerId=10023">Click Here to Sign Up for an Account</a><br /><br /><a rel="noreferrer noopener" target="_blank" href="https://account.authorize.net/">Click to Login to the Authorize.net Merchant Area</a><br /><br /><strong>Requirements:</strong><br /><hr />*<strong>Authorize.net Account</strong> (see link above to signup)<br />*<strong>Authorize.net username and transaction key</strong> available from your Merchant Area<br><br>See <a href="https://docs.zen-cart.com/user/modules/authorizenet_sim/" rel="noreferrer noopener" target="_blank">the SIM Setup FAQ article</a> for detailed setup instructions.',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_TYPE' => 'Type : ',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_CREDIT_CARD_OWNER' => 'Propriétaire de la carte de crédit : ',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_CREDIT_CARD_NUMBER' => 'Numéro de la carte de crédit : ',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_CREDIT_CARD_EXPIRES' => 'Date d\'expiration la carte : ',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_CVV' => 'Numéro CVV : ',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_POPUP_CVV_LINK' => 'Qu\'est-ce que c\'est ?',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_JS_CC_OWNER' => '* Le nom du propriétaire de la carte de crédit doit comporter au moins  '.CC_OWNER_MIN_LENGTH.' caractères.',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_JS_CC_NUMBER' => '* Le numéro de carte doit comporter au moins '.CC_NUMBER_MIN_LENGTH.' caractères.',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_JS_CC_CVV' => '* Les 3 or 4 chiffres du numéro CVV doivent être recopiés à partir du dos de la carte bancaire.',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_ERROR_MESSAGE' => 'Une erreur est survenue lors du traitement de votre carte de crédit. Merci de bien vouloir recommencer.',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_DECLINED_MESSAGE' => 'Votre carte de crédit a été refusée. Veuillez essayer une autre carte ou contacter votre banque pour plus d\'informations.',
+    'MODULE_PAYMENT_AUTHORIZENET_TEXT_ERROR' => 'Erreur de carte de crédit !',
+];
+
+return $define;
