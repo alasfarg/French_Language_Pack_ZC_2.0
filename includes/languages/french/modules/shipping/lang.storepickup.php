0a904341f068745865866e9d3068bdcf52687a45
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:04:01 +0100

    ajout fichiers

---
+++ includes/languages/french/modules/shipping/lang.storepickup.php
@@ -0,0 +1,9 @@
+<?php
+$define = [
+    'MODULE_SHIPPING_STOREPICKUP_TEXT_TITLE' => 'Retrait en Point de Vente',
+    'MODULE_SHIPPING_STOREPICKUP_TEXT_DESCRIPTION' => 'Retrait sur place par le client',
+    'MODULE_SHIPPING_STOREPICKUP_TEXT_WAY' => 'Sur place',
+    'MODULE_SHIPPING_STOREPICKUP_MULTIPLE_WAYS' => "",
+];
+
+return $define;
