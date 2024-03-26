80e849f443898113f7a7abc6b79266616955386c
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:24:08 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.zones.php
@@ -0,0 +1,19 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Zones',
+    'TABLE_HEADING_COUNTRY_NAME' => 'Pays',
+    'TABLE_HEADING_ZONE_NAME' => 'Zone',
+    'TABLE_HEADING_ZONE_CODE' => 'Code',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TEXT_INFO_EDIT_INTRO' => 'Veuillez effectuer les changements nécessaires',
+    'TEXT_INFO_ZONES_NAME' => 'Nom de la zone :',
+    'TEXT_INFO_ZONES_CODE' => 'Code de la zone :',
+    'TEXT_INFO_COUNTRY_NAME' => 'Pays :',
+    'TEXT_INFO_INSERT_INTRO' => 'Veuillez saisir la nouvelle zone et ses données relatives',
+    'TEXT_INFO_DELETE_INTRO' => 'Êtes-vous certain de vouloir supprimer cette zone ?',
+    'TEXT_INFO_HEADING_NEW_ZONE' => 'Nouvelle zone',
+    'TEXT_INFO_HEADING_EDIT_ZONE' => 'Éditer la zone',
+    'TEXT_INFO_HEADING_DELETE_ZONE' => 'Supprimer la zone',
+];
+
+return $define;
