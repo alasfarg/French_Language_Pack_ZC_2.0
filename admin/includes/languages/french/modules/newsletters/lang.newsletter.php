be9aaba701d318c092b42c99b8faeddad07e4169
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:25:10 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/modules/newsletters/lang.newsletter.php
@@ -0,0 +1,25 @@
+<?php
+$define = [
+    'TEXT_COUNT_CUSTOMERS' => 'Clients abonnés au bulletin : %s',
+    'HEADING_TITLE' => 'Gestion des Bulletins',
+    'TABLE_HEADING_NEWSLETTERS' => 'Bulletins',
+    'TABLE_HEADING_SIZE' => 'Taille',
+    'TABLE_HEADING_MODULE' => 'Module',
+    'TABLE_HEADING_SENT' => 'Envoyé',
+    'TABLE_HEADING_STATUS' => 'Statut',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TEXT_NEWSLETTER_MODULE' => 'Module :',
+    'TEXT_NEWSLETTER_TITLE' => 'Sujet :',
+    'TEXT_NEWSLETTER_CONTENT' => 'Contenu <br />\'Text-Only\' :',
+    'TEXT_NEWSLETTER_CONTENT_HTML' => 'Contenu <br />\'Rich Text\' :',
+    'TEXT_NEWSLETTER_DATE_ADDED' => 'Date de création :',
+    'TEXT_NEWSLETTER_DATE_SENT' => 'Date d\'envoi :',
+    'TEXT_INFO_DELETE_INTRO' => 'Êtes-vous certain(e) de vouloir supprimer ce bulletin ?',
+    'TEXT_PLEASE_SELECT_AUDIENCE' => 'Veuillez sélectionner le public de ce bulletin : ',
+    'TEXT_PLEASE_WAIT' => 'Veuillez patienter .. Envoi des mails ..<br /><br />Veuillez ne pas interrompre le processus !',
+    'TEXT_FINISHED_SENDING_EMAILS' => 'Envoi des mails terminé !',
+    'ERROR_NEWSLETTER_TITLE' => 'ERREUR : Intitulé de bulletin requis',
+    'ERROR_NEWSLETTER_MODULE' => 'ERREUR : Module bulletin (newsletter) requis',
+];
+
+return $define;
---
+++ admin/includes/languages/french/modules/newsletters/lang.product_notification.php
@@ -0,0 +1,14 @@
+<?php
+$define = [
+    'TEXT_COUNT_CUSTOMERS' => 'Clients abonnés à cette annonce de produit: %s',
+    'TEXT_PRODUCTS' => 'Produits',
+    'TEXT_SELECTED_PRODUCTS' => 'Produits sélectionnés',
+    'JS_PLEASE_SELECT_PRODUCTS' => 'Veuillez choisir quelques produits',
+    'BUTTON_GLOBAL' => 'Global',
+    'BUTTON_SELECT' => '>>>',
+    'BUTTON_UNSELECT' => '<<<',
+    'BUTTON_SUBMIT' => 'Envoyer',
+    'BUTTON_CANCEL' => 'Annuler',
+];
+
+return $define;
