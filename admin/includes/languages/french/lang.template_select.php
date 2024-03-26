7de23f0fbec8695365d431398faab404cd27aae1
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:23:43 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.template_select.php
@@ -0,0 +1,27 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Sélection du Template',
+    'TEXT_INFO_DEFAULT_LANGUAGE' => 'Par défaut (tous)',
+    'TEXT_TEMPLATE_SELECT_INFO' => '<p>Ici, vous pouvez modifier le template utilisé pour afficher la vitrine.</p><p>Le template par défaut est normalement utilisé pour <strong>toutes</strong> les langues. L\'ajout d\'affectations de template à langue n\'est généralement utile que lorsque vous prenez en charge les langues RTL parallèlement aux langues LTR.</p><p>Il est possible d\'afficher un template dans une session <em>privée</em>, par exemple lors du test d\'un nouveau template : consultez l\'aide en ligne <strong>??</strong></p>',
+    'TABLE_HEADING_LANGUAGE' => 'Langue du Template',
+    'TABLE_HEADING_NAME' => 'Nom du Template',
+    'TABLE_HEADING_DIRECTORY' => 'Répertoire du Template',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TEXT_INFO_HEADING_LANGUAGE' => 'Langue',
+    'TEXT_INFO_DEFAULT_TEMPLATE' => 'Ce modèle est utilisé par défaut / lorsqu\'une langue installée n\'a pas de template attribué.',
+    'TEXT_INFO_HEADING_EDIT_TEMPLATE' => 'Éditer les paramètres du template',
+    'TEXT_INFO_HEADING_DELETE_TEMPLATE' => 'Supprimer l\'association d\'un template',
+    'TEXT_INFO_EDIT_INTRO' => 'Changer de template',
+    'TEXT_INFO_DELETE_INTRO' => 'Supprimer cette association',
+    'TEXT_INFO_TEMPLATE_NAME' => 'Nom du template: ',
+    'TEXT_INFO_LANGUAGE_NAME' => 'Nom de la langue: ',
+    'TEXT_INFO_TEMPLATE_VERSION' => 'Version du template: ',
+    'TEXT_INFO_TEMPLATE_AUTHOR' => 'Auteur du template: ',
+    'TEXT_INFO_TEMPLATE_DESCRIPTION' => 'Description du template: ',
+    'TEXT_INFO_TEMPLATE_INSTALLED' => 'Templates installés: ',
+    'TEXT_INFO_HEADING_NEW_TEMPLATE' => 'Associer un template à une langue',
+    'TEXT_INFO_INSERT_INTRO' => 'Sélectionnez ci-dessous une association template à langue',
+    'IMAGE_NEW_TEMPLATE' => 'Créer une nouvelle association template/langue',
+];
+
+return $define;
