7e543c7793f18c26d2fe932667ba02c5abb808d7
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:15:07 +0100

    ajout fichiers

---
+++ includes/languages/french/html_includes/classic/define_shippinginfo.php
@@ -0,0 +1,5 @@
+<p><strong>Exemple de texte d'expédition ...</strong></p>
+<p>Cette section de texte provient de l'éditeur de définition de pages situé sous Outils dans l'administration.</p>
+<p>Pour supprimer cette section du texte, supprimez-la de l'éditeur de définition de pages.</p>
+<p>Ce fichier se trouve dans<code>/languages/french/html_includes/classic /</code></p>
+<p><strong>REMARQUE: sauvegardez toujours les fichiers dans<code>/languages/french/html_includes/your_template</code></strong></p>
---
+++ includes/languages/french/html_includes/classic/define_shopping_cart.php
@@ -0,0 +1,5 @@
+<p>Location : includes/languages/french/html_includes/classic/define_shopping_cart.php</p>
+<p><strong>Conditions d'utilisation Exemple de texte ...</strong></p>
+<p>Cette section de texte provient de l'éditeur Définir les pages situé sous Outils dans l'Admin.</p>
+<p>Pour supprimer cette section du texte, supprimez-la de l'éditeur de définition de pages.</p>
+
