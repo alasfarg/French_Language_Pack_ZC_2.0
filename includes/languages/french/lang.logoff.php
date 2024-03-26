41d64e84ae08947bbbcbdce1fe0397dbb7f76bc4
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 13:42:10 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.logoff.php
@@ -0,0 +1,8 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Déconnexion',
+    'NAVBAR_TITLE' => 'Déconnexion',
+    'TEXT_MAIN' => 'Vous avez été déconnecté(e) de votre compte. Vous pouvez quitter votre ordinateur en toute sécurité.<br /><br />Si vous aviez des articles dans votre panier, ils ont été sauvegardés. Les articles de votre panier seront restaurés à votre prochaine <a href="'.zen_href_link(FILENAME_LOGIN,'','SSL',
+];
+
+return $define;
