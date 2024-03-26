6b100feef6a6ef67cfb8dbc71cda0bf136001f1d
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:05:39 +0100

    ajout fichiers

---
+++ includes/languages/french/responsive_classic/lang.other_images_names.php
@@ -0,0 +1,28 @@
+<?php
+$define = [
+    'OTHER_IMAGE_BLACK_SEPARATOR' => 'pixel_black.gif',
+    'OTHER_IMAGE_BOX_NOTIFY_REMOVE' => 'box_products_notifications_remove.gif',
+    'OTHER_IMAGE_BOX_NOTIFY_YES' => 'box_products_notifications.gif',
+    'OTHER_IMAGE_BOX_WRITE_REVIEW' => 'box_write_review.gif',
+    'OTHER_IMAGE_CALL_FOR_PRICE' => 'call_for_prices.png',
+    'OTHER_IMAGE_DOWN_FOR_MAINTENANCE' => 'down_for_maintenance.png',
+    'OTHER_IMAGE_PRICE_IS_FREE' => 'free.png',
+    'OTHER_IMAGE_CUSTOMERS_AUTHORIZATION' => 'customer_authorization.gif',
+    'OTHER_IMAGE_REVIEWS_RATING_STARS_FIVE' => 'stars_5_small.png',
+    'OTHER_IMAGE_REVIEWS_RATING_STARS_FOUR' => 'stars_4_small.png',
+    'OTHER_IMAGE_REVIEWS_RATING_STARS_THREE' => 'stars_3_small.png',
+    'OTHER_IMAGE_REVIEWS_RATING_STARS_TWO' => 'stars_2_small.png',
+    'OTHER_IMAGE_REVIEWS_RATING_STARS_ONE' => 'stars_1_small.png',
+    'OTHER_BOX_NOTIFY_REMOVE_ALT' => 'Supprimer cette notification de produit.',
+    'OTHER_BOX_NOTIFY_YES_ALT' => 'M\'avertir des mises à jour de ce produit.',
+    'OTHER_BOX_WRITE_REVIEW_ALT' => 'Rédiger un avis sur ce produit.',
+    'OTHER_DOWN_FOR_MAINTENANCE_ALT' => 'Le site est actuellement indisponible pour maintenance. Veuillez revenir plus tard.',
+    'OTHER_IMAGE_CUSTOMERS_AUTHORIZATION_ALT' => 'L\'APPROBATION DU CLIENT EST EN ATTENTE ...',
+    'OTHER_REVIEWS_RATING_STARS_FIVE_ALT' => 'Cinq étoiles',
+    'OTHER_REVIEWS_RATING_STARS_FOUR_ALT' => 'Quatre étoiles',
+    'OTHER_REVIEWS_RATING_STARS_THREE_ALT' => 'Trois étoiles',
+    'OTHER_REVIEWS_RATING_STARS_TWO_ALT' => 'Deux étoiles',
+    'OTHER_REVIEWS_RATING_STARS_ONE_ALT' => 'Une étoile',
+];
+
+return $define;
