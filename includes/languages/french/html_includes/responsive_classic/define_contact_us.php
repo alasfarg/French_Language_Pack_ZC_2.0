faaf1d1555787a230c7bbe393d65e0a219912602
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:11:41 +0100

    ajout fichiers

---
+++ includes/languages/french/html_includes/responsive_classic/define_contact_us.php
@@ -0,0 +1,5 @@
+<p><strong> Contactez-nous Exemple de texte ...</strong></p>
+<p>Cette section de texte provient de l'éditeur de définition de pages situé sous Outils dans l'administration.</p>
+<p>Pour supprimer cette section du texte, supprimez-la de l'éditeur de définition de pages.</p>
+<p>Ce fichier est <code> / languages / french / html_includes / classic / define_contact_us.php</code></p>
+<p><strong>REMARQUE: sauvegardez toujours les fichiers dans <code>/languages/french/html_includes/your_template</code></strong></p> 
