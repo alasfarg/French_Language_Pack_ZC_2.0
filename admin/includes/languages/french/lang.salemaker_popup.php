f0dac72f9a14dd8be80f401e22eb830c0cc60d10
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:21:55 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.salemaker_popup.php
@@ -0,0 +1,16 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Soldeur',
+    'SUBHEADING_TITLE' => 'Conseils d\'utilisation pour le Soldeur:',
+    'TEXT_SALEMAKER_IMMEDIATELY' => 'Immédiatement',
+    'TEXT_SALEMAKER_NEVER' => 'Jamais',
+    'TEXT_CLOSE_WINDOW' => '[ Fermer la fenêtre ]',
+    'TABLE_HEADING_SALE_NAME' => 'Nom de la solde',
+    'TABLE_HEADING_SALE_DEDUCTION' => 'Déduction',
+    'TABLE_HEADING_SALE_DATE_START' => 'Date de début',
+    'TABLE_HEADING_SALE_DATE_END' => 'Date de fin',
+    'TABLE_HEADING_STATUS' => 'Statut',
+    'TABLE_HEADING_ACTION' => 'Action',
+];
+
+return $define;
---
+++ admin/includes/languages/french/lang.server_info.php
@@ -0,0 +1,28 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Informations Serveur',
+    'TITLE_SERVER_HOST' => 'Hôte du serveur :',
+    'TITLE_SERVER_OS' => 'OS du serveur :',
+    'TITLE_SERVER_DATE' => 'Date du serveur :',
+    'TITLE_SERVER_UP_TIME' => 'Affichage temps d\'exécution du serveur :',
+    'TITLE_HTTP_SERVER' => 'Serveur HTTP :',
+    'TITLE_PHP_VERSION' => 'Version de PHP :',
+    'TITLE_ZEND_VERSION' => 'Zend:',
+    'TITLE_DATABASE_HOST' => 'Hôte de la base de données :',
+    'TITLE_DATABASE' => 'Base de données :',
+    'TITLE_DATABASE_DATE' => 'Date de la base de données :',
+    'TITLE_DATABASE_DATA_SIZE' => 'Taille des données de la base :',
+    'TITLE_DATABASE_INDEX_SIZE' => 'Taille de l\'index de la base :',
+    'TITLE_PHP_MEMORY_LIMIT' => 'Limite mémoire par script :',
+    'TITLE_PHP_FILE_UPLOADS' => 'Upload de fichiers :',
+    'TITLE_PHP_UPLOAD_MAX' => 'Taille max d\'un fichier à uploader :',
+    'TITLE_PHP_POST_MAX_SIZE' => 'Taille max de données POST :',
+    'TITLE_DATABASE_ENGINE' => 'Engine de la base : ',
+    'PROJECT_DATABASE_LABEL' => 'Niveau de patch de la base de données : ',
+    'TITLE_MYSQL_STRICT_MODE' => '(en mode strict)',
+    'TITLE_DATABASE_MYSQL_MODE' => 'Mode MySQL :',
+    'TITLE_DATABASE_MYSQL_SLOW_LOG_STATUS' => 'État du journal des requêtes lentes MySQL :',
+    'TITLE_DATABASE_MYSQL_SLOW_LOG_FILE' => 'Fichier journal des requêtes lentes MySQL :',
+];
+
+return $define;
