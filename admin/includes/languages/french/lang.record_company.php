1dcf9c865cf85e51ec662bcb9dea096276f2d0eb
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:21:22 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.record_company.php
@@ -0,0 +1,26 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Maisons de Disques',
+    'TABLE_HEADING_RECORD_COMPANY' => 'Maison de Disques',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TEXT_NEW_INTRO' => 'Veuillez renseigner les informations concernant la nouvelle maison de disques ci-dessous.',
+    'TEXT_EDIT_INTRO' => 'Veuillez rectifier les informations concernant la maison de disques ci-dessous.',
+    'TEXT_DELETE_INTRO' => 'Êtes-vous certain de vouloir supprimer cette maison de disques ?',
+    'TEXT_DELETE_IMAGE' => 'Supprimer l\'image ?',
+    'TEXT_RECORD_COMPANY_NAME' => 'Nom de la maison de disques',
+    'TEXT_RECORD_COMPANY_IMAGE' => 'Image de la maison de disques',
+    'TEXT_RECORD_COMPANY_URL' => 'Site web de la maison de disques',
+    'TEXT_RECORD_COMPANY_IMAGE_DIR' => 'Répertoire de l\'image&nbsp;',
+    'TEXT_RECORD_COMPANY_IMAGE_MANUAL' => '<strong>Ou, choisir un fichier image existant à partir du serveur, nom du fichier :</strong>',
+    'TEXT_HEADING_NEW_RECORD_COMPANY' => 'Nouvelle maison de disques',
+    'TEXT_HEADING_EDIT_RECORD_COMPANY' => 'Éditer la maison de disques',
+    'TEXT_HEADING_DELETE_RECORD_COMPANY' => 'Supprimer la maison de disques',
+    'TEXT_DATE_ADDED' => 'Date de création :',
+    'TEXT_LAST_MODIFIED' => 'Dernière modification :',
+    'TEXT_PRODUCTS' => 'Produits liés',
+    'TEXT_DISPLAY_NUMBER_OF_RECORD_COMPANIES' => 'Affiche <strong>%d</strong> à <strong>%d</strong> (sur <strong>%d</strong> maisons de disques)',
+    'TEXT_DELETE_PRODUCTS' => 'Supprimer cette maison de disques et tous les articles qui y sont liés ?',
+    'TEXT_DELETE_WARNING_PRODUCTS' => '<b>ATTENTION :</b> Il reste %s article(s) encore associé(s) à cette maison de disques !',
+];
+
+return $define;
