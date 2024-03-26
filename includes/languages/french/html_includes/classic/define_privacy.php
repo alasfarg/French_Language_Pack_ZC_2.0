fb4e8066c9e2e9457da3a9a551ed495fa3642ee5
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:14:53 +0100

    ajout fichiers

---
+++ includes/languages/french/html_includes/classic/define_privacy.php
@@ -0,0 +1,5 @@
+<p><strong> Exemple de texte de confidentialité ... </strong> </p>
+<p>Ce texte peut être modifié dans l'éditeur des pages 'define' situé dans le menu Outils de l'Administration.</p>
+<p>Pour supprimer cette section du texte, supprimez-la de l'éditeur de définition de pages.</p>
+<p>Ce fichier se trouve dans <code>/languages/english/html_includes/classic/</code></p>
+<p><strong>REMARQUE: sauvegardez toujours les fichiers dans <code>/languages/french/html_includes/your_template</code></strong></p> 
