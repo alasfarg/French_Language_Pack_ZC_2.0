8d6ecc96ced61962f3d09042b193d49b47fc3ad9
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:23:07 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.stats_products_viewed.php
@@ -0,0 +1,11 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Produits les plus consultés',
+    'TABLE_HEADING_NUMBER' => 'Produit avec ID#',
+    'TABLE_HEADING_PRODUCTS' => 'Nom de produit (Langue)',
+    'TABLE_HEADING_VIEWED' => 'Vues',
+    'TEXT_REPORT_START_DATE' => 'Date de début',
+    'TEXT_REPORT_END_DATE' => 'Date de fin',
+];
+
+return $define;
---
+++ admin/includes/languages/french/lang.stats_sales_report_graphs.php
@@ -0,0 +1,27 @@
+<?php
+$define = [
+    'FILTER_STATUS' => '<b>Filtrer par état de la commande</b>',
+    'FILTER_VALUE' => '<b>Filtre activé / désactivé</b>',
+    'HEADING_TITLE' => 'Rapports de ventes',
+    'TODAY_TO_DATE' => 'Totaux quotidiens',
+    'WEEK_TO_DATE' => 'Totaux hebdomadaires',
+    'MONTH_TO_DATE' => 'Totaux mensuels',
+    'AVERAGE_ORDER' => 'Montant moyen de la commande',
+    'CHART_TOTAL_SALES' => 'Ventes totales',
+    'CHART_AVERAGE_SALE_AMOUNT' => 'Montant moyen des ventes',
+    'TEXT_PREVIOUS_LINK' => '&lt;&lt;&nbsp;Précédent',
+    'TEXT_NEXT_LINK' => 'Suivant&nbsp;&gt;&gt;',
+    'REPORT_TEXT_ORDERS' => 'Commandes',
+    'REPORT_TEXT_CONVERSION_PER_ORDER' => 'Conversion par commande',
+    'REPORT_TEXT_CONVERSION' => 'Conversion',
+    'REPORT_TEXT_VARIANCE' => 'Variance',
+    'REPORT_TEXT_HOURLY' => 'Horaire',
+    'REPORT_TEXT_DAILY' => 'Quotidien',
+    'REPORT_TEXT_WEEKLY' => 'Hebdomadaire',
+    'REPORT_TEXT_MONTHLY' => 'Mensuel',
+    'REPORT_TEXT_YEARLY' => 'Annuel',
+    'TOTAL_ANNÉE' => 'Total annuel',
+    'CHART_TEXT_AVERAGE' => 'Moyenne',
+];
+
+return $define;
