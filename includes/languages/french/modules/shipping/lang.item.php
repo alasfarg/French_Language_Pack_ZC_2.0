e0ce5a7e9d16179ff3dcd2f9c12d5483fd5b1fb1
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:03:33 +0100

    ajout fichiers

---
+++ includes/languages/french/modules/shipping/lang.item.php
@@ -0,0 +1,8 @@
+<?php
+$define = [
+    'MODULE_SHIPPING_ITEM_TEXT_TITLE' => 'Par Article',
+    'MODULE_SHIPPING_ITEM_TEXT_DESCRIPTION' => 'Par article',
+    'MODULE_SHIPPING_ITEM_TEXT_WAY' => 'La meilleure façon',
+];
+
+return $define;
