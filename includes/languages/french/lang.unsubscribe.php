16bad03e94c82a83e565e5a24eaf764c01d965cd
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 18:56:58 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.unsubscribe.php
@@ -0,0 +1,11 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Désabonnement',
+    'HEADING_TITLE' => 'Désabonnement de notre bulletin',
+    'UNSUBSCRIBE_TEXT_INFORMATION' => '<br />Nous sommes désolés d\'apprendre que vous souhaitez vous désabonner de notre bulletin. Si vous avez des inquiétudes concernant votre vie privée, nous vous invitons à prendre connaissance de notre <a href="'.zen_href_link(FILENAME_PRIVACY,'','NONSSL',
+    'UNSUBSCRIBE_TEXT_NO_ADDRESS_GIVEN' => '<br />Nous sommes désolés d\'apprendre que vous souhaitez vous désabonner de notre bulletin. Si vous avez des inquiétudes concernant votre vie privée, nous vous invitons à prendre connaissance de notre <a href="'.zen_href_link(FILENAME_PRIVACY,'','NONSSL',
+    'UNSUBSCRIBE_DONE_TEXT_INFORMATION' => '<br />Votre adresse e-mail, indiquée ci-dessous, a été supprimé de notre bulletin, ainsi que vous l\'avez demandé.<br /><br />',
+    'UNSUBSCRIBE_ERROR_INFORMATION' => '<br />L\'adresse e-mail que vous avez indiquée ne figure pas dans la base de données de notre bulletin, ou a déjà été effacée de notre liste d\'abonnés au bulletin. <br /><br />',
+];
+
+return $define;
