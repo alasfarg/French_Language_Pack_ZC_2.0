53d4420ff10f95f45d0c513e09e298de2d6691d3
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:20:04 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.products_expected.php
@@ -0,0 +1,10 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Produits attendus',
+    'TABLE_HEADING_PRODUCTS' => 'Produits',
+    'TABLE_HEADING_DATE_EXPECTED' => 'Date prévue',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TEXT_INFO_DATE_EXPECTED' => 'Date prévue:',
+];
+
+return $define;
