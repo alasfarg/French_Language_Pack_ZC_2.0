8dbe79aa644207758d5792d78ad174bf094aa3b0
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:08:51 +0100

    ajout fichiers

---
+++ includes/languages/french/html_includes/define_about_us.php
@@ -0,0 +1,3 @@
+<p>Nous sommes beaucoup plus intéressant que cette page blanche!</p>
+<p>Merci d'utiliser la page Contact-Us pour nous rappeler de mettre à jour cette page.</p>
+<p class="small pt-5"><br><br><br>Mainteneurs: utiliseze le Define Pages editeur dans votre Admin pour mettre à jour ce texte.</p>
