a4902f0626683a35a2de03b1af2a2b53cf425d24
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 18:56:38 +0100

    ajout fichiers

---
+++ includes/languages/french/lang.ssl_check.php
@@ -0,0 +1,14 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Vérification de Sécurité',
+    'HEADING_TITLE' => 'Vérification de sécurité',
+    'TEXT_INFORMATION' => 'Nous avons détecté que votre navigateur a généré un ID de session SSL différent de celui utilisé sur nos pages sécurisées.',
+    'TEXT_INFORMATION_2' => 'Pour des questions de sécurité, vous devez vous reconnecter à votre compte afin de poursuivre vos achats en ligne.',
+    'TEXT_INFORMATION_3' => 'Certains navigateurs, comme Konqueror 3.1, n\'offrent pas la possibilité de générer automatiquement l\'ID de session SSL sécurisée nécessaire. Si vous utilisez un tel navigateur, nous vous recommandons de passer à un autre navigateur pour continuer vos achats en ligne avec nous. Si vous n\'avez pas d\'autre navigateur installé sur votre ordinateur, vous pouvez en télécharger un compatible depuis: <a href="http://www.microsoft.com/ie/" target="_blank">Microsoft Internet Explorer</a>, <a href="http://channels.netscape.com/ns/browsers/download_other.jsp" target="_blank">Netscape</a>, ou <a href="http://www.mozilla.org/releases/" target="_blank">Mozilla</a>.',
+    'TEXT_INFORMATION_4' => 'Nous avons pris cette mesure pour votre intérêt; et nous nous excusons pour la gêne occasionnée.',
+    'TEXT_INFORMATION_5' => 'Veuillez contacter le gérant de la boutique pour toute question relative à cette exigence, ou pour continuer à effectuer vos achats hors ligne.',
+    'BOX_INFORMATION_HEADING' => 'Sécurité et confidentialité',
+    'BOX_INFORMATION' => 'Nous validons automatiquement l\'ID de session SSL généré par votre navigateur à chaque demande de page sécurisée faite sur ce serveur.<br /><br />Cette validation permet de nous assurer que c\'est bien vous qui naviguez sur ce site avec votre compte et non quelqu\'un d\'autre.',
+];
+
+return $define;
---
+++ includes/languages/french/lang.time_out.php
@@ -0,0 +1,12 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Déconnexion Automatique',
+    'HEADING_TITLE' => 'Ouups! Votre session a expiré',
+    'HEADING_TITLE_LOGGED_IN' => 'Ouups! Désolé, mais vous n\'êtes pas autorisé(e) à effectuer l\'action demandée. ',
+    'TEXT_INFORMATION' => '<p>Si vous êtiez en train de passer commande, veuillez vous connecter et votre panier sera restauré. Vous pourrez alors revenir à l\'encaissement et terminer votre achat final.</p><p>Si vous aviez terminé une commande et vous souhaitez la contôler'.(DOWNLOAD_ENABLED'true'?', ou si vous aviez un téléchargement et souhaitez le reprendre':'',
+    'TEXT_INFORMATION_LOGGED_IN' => 'Vous êtes toujours connecté à votre compte et vous pouvez continuer vos achats. Veuillez choisir une destination à partir du menu.',
+    'HEADING_RETURNING_CUSTOMER' => 'Connexion',
+    'TEXT_PASSWORD_FORGOTTEN' => 'Mot de passe oublié ?',
+];
+
+return $define;
