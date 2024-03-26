b6429102915ac519fc1300e3224f17d1b814a2ac
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:10:29 +0100

    ajout fichiers

---
+++ includes/languages/french/html_includes/define_privacy.php
@@ -0,0 +1,6 @@
+<p><strong>Exemple de texte de confidentialité ...</strong></p>
+<p>Cette section de texte provient de l'éditeur de définition de pages situé sous Outils dans l'administration.</p>
+<p>Pour supprimer cette section du texte, supprimez-la de l'éditeur de définition de pages.</p>
+
+
+
---
+++ includes/languages/french/html_includes/define_shippinginfo.php
@@ -0,0 +1,3 @@
+<p><strong>Livraisons - Exemple de texte ...</strong></p>
+<p>Ce texte peut &ecirc;tre modifi&eacute; avec l'&eacute;diteur des pages 'define' situ&eacute; dans le menu Outils de l'Administration.</p>
+<p>Pour enlever ce texte, supprimez le avec ce m&ecirc;me &eacute;diteur de pages.</p>
