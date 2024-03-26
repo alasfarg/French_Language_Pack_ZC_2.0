29627d4c95110212f38a9466f87a2c031d3b5259
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:22:39 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.stats_customers.php
@@ -0,0 +1,9 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Total Commandé par Client',
+    'TABLE_HEADING_NUMBER' => 'ID#',
+    'TABLE_HEADING_CUSTOMERS' => 'Clients',
+    'TABLE_HEADING_TOTAL_PURCHASED' => 'Total acheté',
+];
+
+return $define;
---
+++ admin/includes/languages/french/lang.stats_customers_referrals.php
@@ -0,0 +1,12 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Parrainages Clients',
+    'TEXT_INFO_SELECT_REFERRAL' => 'Sélectionnez un code de parrainage / coupon de réduction',
+    'TEXT_REFERRAL_UNKNOWN' => 'Inconnu',
+    'TEXT_INFO_START_DATE' => 'Date de début (d-m-y)',
+    'TEXT_INFO_END_DATE' => 'Date de fin (d-m-y)',
+    'TEXT_ORDER_NUMBER' => 'Commande #',
+    'TEXT_COUPON_ID' => 'coupon de réduction ID#',
+];
+
+return $define;
