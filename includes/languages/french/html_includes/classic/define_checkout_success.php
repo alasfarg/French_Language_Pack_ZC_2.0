f4f23a166df7e6d0d7574c23a4981be7109f5f71
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:13:56 +0100

    ajout fichiers

---
+++ includes/languages/french/html_includes/classic/define_checkout_success.php
@@ -0,0 +1,5 @@
+<p><strong>Encaissement réussi - Exemple de texte...</strong></p>
+<p>Quelques mots au sujet du délai de livraison ou de votre politique de traitement des commandes peuvent être mis ici.</p>
+<p>Ce texte peut être modifié dans l'éditeur des pages 'define' situé dans le menu Outils de l'Administration.</p>
+<p>Ce fichier se situe dans <code>/languages/french/html_includes/classic/</code></p>
+<p><strong>NOTE: Sauvegardez toujours les fichiers dans <code>/languages/french/html_includes/your_template</code></strong></p>
