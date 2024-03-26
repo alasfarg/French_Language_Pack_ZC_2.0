e693f33d48338b907d06b9314d266327926ec364
andre <andre.lasfargues@iut-rodez.fr>
2024-03-26 19:09:30 +0100

    ajout fichiers

---
+++ includes/languages/french/html_includes/define_contact_us.php
@@ -0,0 +1,3 @@
+<p<<strong> Contactez-nous Exemple de texte ...</strong></p>
+<p> Cette section de texte provient de l'éditeur de définition de pages situé sous Outils dans l'administration.</p>
+<p> Pour supprimer cette section du texte, supprimez-la de l'éditeur de définition de pages.</p>
---
+++ includes/languages/french/html_includes/define_discount_coupon.php
@@ -0,0 +1,5 @@
+<p><strong>Bon de réduction - Exemple de texte ...</strong></p>
+<p>Ce texte peut être modifié dans l'éditeur des pages 'define' situé dans le menu Outils de l'Administration.</p>
+<p>Pour enlever ce texte, supprimez le avec ce même éditeur de pages.</p>
+
+
