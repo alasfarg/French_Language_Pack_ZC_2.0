7936b0efb066fb60e29c8fb786fdbf1f3405b676
andre <andre.lasfargues@iut-rodez.fr>
2024-03-27 17:56:04 +0100

    ajout fichier

---
+++ admin/includes/languages/french/lang.coupon_referrers.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: DrByte 2024 Feb 23 New in v2.0.0-beta1 $
+*/
+
+$define = [
+    'HEADING_TITLE' => 'Référents de coupons',
+    'TABLE_HEADING_REFERRER_DOMAIN' => 'Domaine référent',
+    'TABLE_HEADING_REFERRER_COUPON_NAME' => 'Coupon',
+    'TEXT_SELECT_A_COUPON' => 'Choisissez un coupon',
+    'TEXT_HEADING_NEW_REFERRER' => 'Nouveau Référent',
+    'TEXT_HEADING_EDIT_REFERRER' => 'Editer le Référent',
+    'TEXT_HEADING_DELETE_REFERRER' => 'Supprimmer le Référent',
+    'TEXT_NEW_INTRO' => 'Veuillez saisir un domaine et un coupon associé',
+    'TEXT_DELETE_INTRO' => 'Êtes-vous sûr de vouloir supprimer ce référent?',
+    'TEXT_DELETE_REFERRER' => 'Supprimmer le Référent',
+    'TEXT_REFERRER_DOMAIN' => 'Domaine référent: ',
+    'TEXT_NO_REFERRERS_FOUND' => 'Aucun référent trouvé. Cliquez sur INSÉRER pour en ajouter un.',
+    'TEXT_DISPLAY_NUMBER_OF_REFERRERS' => 'Affichage de <strong>%d</strong> à <strong>%d</strong> (sur <strong>%d</strong> référents)',
+    'ERROR_REFERRER_DOMAIN_UNIQUE_REQUIRED' => 'ERREUR : le domaine référent est obligatoire et doit être unique.',
+    'ERROR_REFERRER_DOMAIN_ALREADY_EXISTS' => 'ERREUR : le domaine référent existe déjà. Impossible d'ajouter des domaines en double.',
+    'ERROR_COUPON_SELECTION_REQUIRED' => 'ERREUR : Vous devez sélectionner un coupon à attribuer au domaine référent.',
+    'ERROR_INVALID_COUPON_SPECIFIED' => 'ERREUR : Coupon non valide sélectionné.',
+];
+
+return $define;
