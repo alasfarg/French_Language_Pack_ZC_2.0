2330eea4092295ffbab75f1c36284d56f7687730
andre <andre.lasfargues@iut-rodez.fr>
2024-03-27 18:07:35 +0100

    ajout fichier

---
+++ admin/includes/auto_loaders/config.french_language_pack.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * French Language Pack
+ * @version 2.0.0 rc2
+ * @author Zen4All with minor adaptions for the French version by André Lasfargues
+ * @copyright (c) 2014-2019, Zen4All
+ * @license http://www.gnu.org/licenses/gpl.txt GNU General Public License V2.0
+ */
+$autoLoadConfig[999][] = array(
+  'autoType' => 'init_script',
+  'loadFile' => 'init_french_language_pack.php');
