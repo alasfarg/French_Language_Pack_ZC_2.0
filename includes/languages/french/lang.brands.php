65cd8ab5412cb869ed978b5686ea00e34b81f975
andre <andre.lasfargues@iut-rodez.fr>
2024-03-27 17:25:38 +0100

    ajout fichier

---
+++ includes/languages/french/lang.brands.php
@@ -0,0 +1,11 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => 'Marques',
+    'HEADING_TITLE' => 'Magasiner par marque',
+    'BREADCRUMB_BRANDS' => 'Magasiner par marque',
+    'FEATURED_BRANDS' => 'Marques en vedette',
+    'OTHER_BRANDS' => 'Marques associées',
+    'NO_BRANDS_AVAILABLE' => 'Aucune marque n\'est actuellement active pour ce magasin.',
+];
+
+return $define;
