a5f1f7fc376e368f665b6ded7b522c67e45d8a27
andre <andre.lasfargues@iut-rodez.fr>
2024-03-27 13:09:05 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.configuration_validation.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'TEXT_EMAIL_ADDRESS_VALIDATE' => 'Le texte saisi ne se résout pas en une adresse e-mail acceptable. (Par exemple, Nom &lt;email@domain&gt; ou &lt;email@domain&gt; ou email@domain ou toute combinaison de ceux-ci séparés par une virgule.)',
+    'TEXT_BOOLEAN_VALIDATE' => 'La valeur doit être une valeur booléenne ou équivalente.',
+];
+
+return $define;
