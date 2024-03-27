8655a794f7b2696e9ceb90147da4036b94f24080
andre <andre.lasfargues@iut-rodez.fr>
2024-03-27 13:10:46 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.currencies.php
@@ -0,0 +1,29 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Devises',
+    'TABLE_HEADING_CURRENCY_NAME' => 'Devise',
+    'TABLE_HEADING_CURRENCY_CODES' => 'Code',
+    'TABLE_HEADING_CURRENCY_VALUE' => 'Valeur',
+    'TABLE_HEADING_ACTION' => 'Action',
+    'TEXT_INFO_EDIT_INTRO' => 'Veuillez effectuer les changements nécessaires',
+    'TEXT_INFO_CURRENCY_TITLE' => 'Intitulé :',
+    'TEXT_INFO_CURRENCY_CODE' => 'Code :',
+    'TEXT_INFO_CURRENCY_SYMBOL_LEFT' => 'Symbole à gauche :',
+    'TEXT_INFO_CURRENCY_SYMBOL_RIGHT' => 'Symbole à droite :',
+    'TEXT_INFO_CURRENCY_DECIMAL_POINT' => 'Point décimal :',
+    'TEXT_INFO_CURRENCY_THOUSANDS_POINT' => 'Séparateur des milliers :',
+    'TEXT_INFO_CURRENCY_DECIMAL_PLACES' => 'Nombre de décimales :',
+    'TEXT_INFO_CURRENCY_LAST_UPDATED' => 'Dernière mise à jour :',
+    'TEXT_INFO_CURRENCY_VALUE' => 'Valeur :',
+    'TEXT_INFO_CURRENCY_EXAMPLE' => 'Exemple :',
+    'TEXT_INFO_INSERT_INTRO' => 'Veuillez renseigner la nouvelle devise et ses données relatives',
+    'TEXT_INFO_DELETE_INTRO' => 'Êtes-vous certain de vouloir supprimer cette devise ?',
+    'TEXT_INFO_HEADING_NEW_CURRENCY' => 'Nouvelle devise',
+    'TEXT_INFO_HEADING_EDIT_CURRENCY' => 'Éditer la devise',
+    'TEXT_INFO_HEADING_DELETE_CURRENCY' => 'Supprimer la devise',
+    'TEXT_INFO_SET_AS_DEFAULT' => 'définir comme défaut  (requiert une actualisation manuelle des taux de change)',
+    'ERROR_REMOVE_DEFAULT_CURRENCY' => 'ERREUR : La devise par défaut ne peut être supprimée. Veuillez définir une autre devise par défaut et réessayer.',
+    'ERROR_INVALID_CURRENCY_ENTRY' => 'ERREUR : Les informations que vous avez entré étaient incomplètes, et n\'ont pas été sauvegardées. Vous devez indiquer un code devise et un nom.',
+];
+
+return $define;
