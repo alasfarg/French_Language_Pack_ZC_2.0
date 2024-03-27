7b83db1a920c40be5f6b40de12ba228bef6ded11
andre <andre.lasfargues@iut-rodez.fr>
2024-03-27 13:07:22 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.banner_statistics.php
@@ -0,0 +1,20 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Statistiques des Bannières',
+    'TABLE_HEADING_SOURCE' => 'Source',
+    'TABLE_HEADING_VIEWS' => 'Affichages',
+    'TABLE_HEADING_CLICKS' => 'Clics',
+    'TEXT_BANNERS_BANNER_VIEWS' => 'Impressions de bannières',
+    'TEXT_BANNERS_BANNER_CLICKS' => 'Clics sur la bannière',
+    'TEXT_BANNERS_DAILY_STATISTICS' => '%s: Statistiques quotidiennes pour %s %s',
+    'TEXT_BANNERS_MONTHLY_STATISTICS' => '%s: Statistiques mensuelles pour %s',
+    'TEXT_BANNERS_YEARLY_STATISTICS' => '%s: Statistiques annuelles',
+    'STATISTICS_TYPE_DAILY' => 'Quotidiennes',
+    'STATISTICS_TYPE_MONTHLY' => 'Mensuelles',
+    'STATISTICS_TYPE_YEARLY' => 'Annuelles',
+    'TITLE_TYPE' => 'Type :',
+    'TITLE_YEAR' => 'Année :',
+    'TITLE_MONTH' => 'Mois :',
+];
+
+return $define;
