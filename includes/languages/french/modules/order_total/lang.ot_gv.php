a0e00a90cf775cda3b75cb4ae8a03b4e75b7e995
andre <andre.lasfargues@iut-rodez.fr>
2024-03-27 17:07:31 +0100

    correct lignes from english

--- includes/languages/french/modules/order_total/lang.ot_gv.php
+++ includes/languages/french/modules/order_total/lang.ot_gv.php
@@ -8,7 +8,7 @@ $define = [
     'TEXT_INVALID_REDEEM_AMOUNT' => 'Le montant que vous avez essayé d\'appliquer et le reliquat disponible de vos chèques cadeaux ne correspondent pas. Veuillez rectifier et réessayer.',
     'MODULE_ORDER_TOTAL_GV_USER_BALANCE' => 'Solde disponible: ',
     'MODULE_ORDER_TOTAL_GV_REDEEM_INSTRUCTIONS' => '<p>Pour utiliser le reliquat disponible de vos chèques cadeaux dans votre compte, tapez le montant que vous souhaitez utiliser dans le champ \'Appliquer montant\. Vous devrez choisir un moyen de paiement, cliquez alors sur le bouton Continuer pour appliquer le reliquat à votre panier.</p><p>Si vous utilisez un <em>nouveau</em> chèque cadeau, vous devez taper le code dans le champ à côté du code de réduction. Le montant de la réduction sera ajouté à votre compte quand vous cliquerez sur le bouton "Continuer".</p>',
-    'MODULE_ORDER_TOTAL_GV_INCLUDE_ERROR' => ' Définir Inclure taxes = true, va seulement être utile si recalculer = None',
+    'MODULE_ORDER_TOTAL_GV_INCLUDE_ERROR' => 'Le paramètre Inclure la taxe = vrai, ne devrait se produire que lorsque recalculer = Aucun',
 ];
 
 return $define;
