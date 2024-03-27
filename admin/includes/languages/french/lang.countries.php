3a4bb8c6e6a645e2f03ef7976d37be0e82172df1
andre <andre.lasfargues@iut-rodez.fr>
2024-03-27 13:09:40 +0100

    ajout fichiers

---
+++ admin/includes/languages/french/lang.countries.php
@@ -0,0 +1,23 @@
+<?php
+$define = [
+    'HEADING_TITLE' => 'Pays',
+    //'TABLE_HEADING_COUNTRY_NAME' => 'Pays',
+    'TABLE_HEADING_COUNTRY_CODES' => 'Codes ISO',
+    'TABLE_HEADING_COUNTRY_STATUS' => 'Statut',
+    //'TABLE_HEADING_ACTION' => 'Action',
+    //'TEXT_INFO_EDIT_INTRO' => 'Veuillez effectuer les changements nécessaires',
+    'TEXT_INFO_COUNTRY_NAME' => 'Nom:',
+    'TEXT_INFO_COUNTRY_CODE_2' => 'Code ISO (2) :',
+    'TEXT_INFO_COUNTRY_CODE_3' => 'Code ISO (3) :',
+    'TEXT_INFO_ADDRESS_FORMAT' => 'Format de l\'adresse :',
+    'TEXT_INFO_COUNTRY_STATUS' => 'Statut (Actif pour l\'expédition ?) :',
+    'TEXT_INFO_INSERT_INTRO' => 'Veuillez saisir le nouveau pays et ses données relatives',
+    'TEXT_INFO_DELETE_INTRO' => 'Êtes-vous certain(e) de vouloir supprimer ce pays ?',
+    'TEXT_INFO_HEADING_NEW_COUNTRY' => 'Nouveau pays',
+    'TEXT_INFO_HEADING_EDIT_COUNTRY' => 'Éditer pays',
+    'TEXT_INFO_HEADING_DELETE_COUNTRY' => 'Supprimer pays',
+    'ERROR_COUNTRY_IN_USE' => 'ERREUR: Impossible de supprimer le pays sélectionné car il est utilisé par des enregistrements clients.',
+    'ISO_COUNTRY_CODES_LINK' => '<a href="https://fr.wikipedia.org/wiki/ISO_3166-1" rel="noreferrer noopener" target="_blank">ISO 3166 Country Codes Reference</a>',
+];
+
+return $define;
