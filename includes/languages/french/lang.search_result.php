a33135c4f81141fa797a1c5d125b43dcf0c2ee0c
andre <andre.lasfargues@iut-rodez.fr>
2024-03-27 17:37:53 +0100

    ajout fichier

---
+++ includes/languages/french/lang.search_result.php
@@ -0,0 +1,14 @@
+<?php
+$define = [
+    'NAVBAR_TITLE_1' => 'Recherche',
+    'NAVBAR_TITLE_2' => 'Résultats de recherche',
+    'HEADING_TITLE' => 'Recherche',
+    'HEADING_SEARCH_CRITERIA' => 'Critères de recherche',
+    'ENTRY_CATEGORIES' => 'Categories:',
+    'ENTRY_MANUFACTURERS' => 'Fabricants:',
+    'TABLE_HEADING_IMAGE' => '',
+    'TEXT_NO_PRODUCTS' => 'Il ya aucun produit correspondant aux critères de recherche.',
+    'TEXT_SHOW' => 'Trier par:',
+];
+
+return $define;
