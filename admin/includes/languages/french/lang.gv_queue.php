1203fd2e0d6c57f461b98e86855897ee352d0a45
andre <andre.lasfargues@iut-rodez.fr>
2024-04-09 14:47:02 +0200

    correct lignes from english

--- admin/includes/languages/french/lang.gv_queue.php
+++ admin/includes/languages/french/lang.gv_queue.php
@@ -1,13 +1,11 @@
 <?php
 $define = [
     'HEADING_TITLE' => '%%TEXT_GV_NAME%%' . ' Queue de déblocage',
-    'TABLE_HEADING_CUSTOMERS' => 'Clients',
     'TABLE_HEADING_ORDERS_ID' => 'Commande n&deg;',
     'TABLE_HEADING_VOUCHER_VALUE' => 'Valeur' . '%%TEXT_GV_NAME%%',
     'TABLE_HEADING_DATE_PURCHASED' => 'Date d\'achat',
-    'TABLE_HEADING_ACTION' => 'Action',
-    'TEXT_REDEEM_GV_MESSAGE_HEADER' => 'Vous avez récemment acheté un' . '%%TEXT_GV_NAME%%' .' sur notre boutique en ligne.',
-    'TEXT_REDEEM_GV_MESSAGE_RELEASED' => 'Pour des questions de sécurité, celui-ci ne vous a pas été immédiatement crédité.'.'Cependant, ce montant a maintenant été débloqué. Vous pouvez vous rendre sur notre site, vous connecter, et adresser la valeur de votre '.%%TEXT_GV_NAME%%' à la personne de votre choix via un e-mail, ou simplement utiliser ce montant pour vous-même.'."\n\n",
+    'TEXT_REDEEM_GV_MESSAGE_HEADER' => 'Vous avez récemment acheté un' . '%%TEXT_GV_NAME%%' . ' sur notre boutique en ligne.',
+    'TEXT_REDEEM_GV_MESSAGE_RELEASED' => 'Pour des raisons de sécurité, cette information n\'a pas été immédiatement mise à votre disposition.',
     'TEXT_REDEEM_GV_MESSAGE_AMOUNT' => 'Le ' . '%%TEXT_GV_NAME%%' . 'que vous avez acheté est d\'une valeur de %s',
     'TEXT_REDEEM_GV_MESSAGE_THANKS' => 'Nous vous remercions pour votre confiance !',
     'TEXT_REDEEM_GV_MESSAGE_BODY' => '',
