5db89107eaf39e570a1f983ba0f3002096f3ce9e
andre <andre.lasfargues@iut-rodez.fr>
2024-04-09 14:15:56 +0200

    correction

--- includes/languages/french/lang.order_status.php
+++ includes/languages/french/lang.order_status.php
@@ -2,7 +2,7 @@
 $define = [
     'NAVBAR_TITLE' => 'Statut de la commande',
     'NAVBAR_TITLE_1' => 'Mon Compte',
-    'HEADING_TITLE' => 'Vérifier l'historique des commandes',
+    'HEADING_TITLE' => 'Vérifier l\'historique des commandes',
 ];
 
 return $define;
