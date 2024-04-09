8955f07193297a2c5632a8d68c836587c330c1b3
andre <andre.lasfargues@iut-rodez.fr>
2024-04-09 14:35:21 +0200

    correct lignes from english

--- includes/languages/french/modules/payment/lang.moneyorder.php
+++ includes/languages/french/modules/payment/lang.moneyorder.php
@@ -4,7 +4,7 @@ $define = [
     'MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION' => 'Les clients peuvent envoyer leur paiement par courrier. Leur e-mail de confirmation de commande leur demandera de : <br /><br />Veuillez établir votre chèque ou virement bancaire à l\'ordre de :<br />'.(defined('%%MODULE_PAYMENT_MONEYORDER_PAYTO%%')),
 ];
  if (defined('MODULE_PAYMENT_MONEYORDER_STATUS')) {   
-    'MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER' => "Veuillez établir votre chèque ou virement bancaire à l\'ordre de :"."\n\n".MODULE_PAYMENT_MONEYORDER_PAYTO."\n\nEnvoyez votre paiement à :\n".STORE_NAME_ADDRESS."\n\n".'Votre commande ne sera envoyée qu\'à réception du règlement et de sa validation par notre banque.',
+    'MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER' = 'Veuillez libeller votre chèque ou mandat à l\'ordre de :' . "\n\n" . MODULE_PAYMENT_MONEYORDER_PAYTO . "\n\n" . 'Envoyez votre paiement à :' . "\n" . STORE_NAME_ADDRESS . "\n\n" . 'Votre commande ne sera pas expédiée tant que nous n\'aurons pas reçu le paiement.';
  }
 
 return $define;
