58e373844e5e93fa28b76c8dc98807324db343e3
andre <andre.lasfargues@iut-rodez.fr>
2024-04-09 14:24:25 +0200

    correct lignes from english

--- includes/languages/french/lang.time_out.php
+++ includes/languages/french/lang.time_out.php
@@ -3,10 +3,9 @@ $define = [
     'NAVBAR_TITLE' => 'Déconnexion Automatique',
     'HEADING_TITLE' => 'Ouups! Votre session a expiré',
     'HEADING_TITLE_LOGGED_IN' => 'Ouups! Désolé, mais vous n\'êtes pas autorisé(e) à effectuer l\'action demandée. ',
-    'TEXT_INFORMATION' => '<p>Si vous êtiez en train de passer commande, veuillez vous connecter et votre panier sera restauré. Vous pourrez alors revenir à l\'encaissement et terminer votre achat final.</p><p>Si vous aviez terminé une commande et vous souhaitez la contôler'.(DOWNLOAD_ENABLED'true'?', ou si vous aviez un téléchargement et souhaitez le reprendre':'',
+    'TEXT_INFORMATION' => '<p>Si vous êtiez en train de passer commande, veuillez vous connecter et votre panier sera restauré. Vous pouvez ensuite revenir à la caisse et finaliser vos derniers achats.</p><p>Si vous avez finalisé une commande et souhaitez la consulter' . (DOWNLOAD_ENABLED == 'true' ? ', ou vous avez effectué un téléchargement et souhaitez le récupérer' : '') . ', veuillez vous rendre sur votre page <a href="' . zen_href_link(FILENAME_ACCOUNT) . '">Mon compte</a> pour consulter votre commande.</p>',
     'TEXT_INFORMATION_LOGGED_IN' => 'Vous êtes toujours connecté à votre compte et vous pouvez continuer vos achats. Veuillez choisir une destination à partir du menu.',
     'HEADING_RETURNING_CUSTOMER' => 'Connexion',
-    'TEXT_PASSWORD_FORGOTTEN' => 'Mot de passe oublié ?',
 ];
 
 return $define;
