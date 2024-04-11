9a59c8a4f6b4a95bb27e24eb4477c683c7787631
alasfarg <44900615+alasfarg@users.noreply.github.com>
2024-04-11 14:24:20 +0200

    Update lang.flat.php

--- includes/languages/french/modules/shipping/lang.flat.php
+++ includes/languages/french/modules/shipping/lang.flat.php
@@ -1,8 +1,8 @@
 <?php
 $define = [
     'MODULE_SHIPPING_FLAT_TEXT_TITLE' => 'Au Forfait',
-    'MODULE_SHIPPING_FLAT_TEXT_DESCRIPTION' => 'Forfait de livraison',
-    'MODULE_SHIPPING_FLAT_TEXT_WAY' => 'La meilleure façon',
+    'MODULE_SHIPPING_FLAT_TEXT_DESCRIPTION' => 'Forfait de Livraison',
+    'MODULE_SHIPPING_FLAT_TEXT_WAY' => 'La meilleure faÃ§on',
 ];
 
 return $define;
