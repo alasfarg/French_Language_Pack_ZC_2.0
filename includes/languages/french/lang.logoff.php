c2acd8a9c962b460d373a36769f3042373c266dc
alasfarg <44900615+alasfarg@users.noreply.github.com>
2024-04-11 10:51:06 +0200

    Update lang.logoff.php

--- includes/languages/french/lang.logoff.php
+++ includes/languages/french/lang.logoff.php
@@ -2,7 +2,7 @@
 $define = [
     'HEADING_TITLE' => 'Déconnexion',
     'NAVBAR_TITLE' => 'Déconnexion',
-    'TEXT_MAIN' => 'Vous avez été déconnecté(e) de votre compte. Vous pouvez quitter votre ordinateur en toute sécurité.<br /><br />Si vous aviez des articles dans votre panier, ils ont été sauvegardés. Les articles de votre panier seront restaurés à votre prochaine <a href="'.zen_href_link(FILENAME_LOGIN) . '"><span class="pseudolink">reconnectez-vous à votre compte</span></a>.<br>',
+    'TEXT_MAIN' => 'Vous avez été déconnecté(e) de votre compte. Vous pouvez quitter votre ordinateur en toute sécurité.<br /><br />Si vous aviez des articles dans votre panier, ils ont été sauvegardés. Les articles de votre panier seront restaurés à votre prochaine <a href="' . zen_href_link(FILENAME_LOGIN) . '"><span class="pseudolink">reconnectez-vous à votre compte</span></a>.<br>',
 ];
 
 return $define;
