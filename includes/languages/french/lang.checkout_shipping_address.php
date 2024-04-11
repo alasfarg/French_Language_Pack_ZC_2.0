0174fcfff869e79aa29e8f39a2f2490bce6c3d2d
alasfarg <44900615+alasfarg@users.noreply.github.com>
2024-04-11 10:58:23 +0200

    Update lang.checkout_shipping_address.php

--- includes/languages/french/lang.checkout_shipping_address.php
+++ includes/languages/french/lang.checkout_shipping_address.php
@@ -4,7 +4,6 @@ $define = [
     'NAVBAR_TITLE_1' => 'Encaissement',
     'NAVBAR_TITLE_2' => 'Modifier l\'adresse de livraison',
     'HEADING_TITLE' => 'Modifier l\'adresse de livraison',
-    //'TABLE_HEADING_SHIPPING_ADDRESS' => 'Adresse de livraison',
     'TITLE_SHIPPING_ADDRESS' => 'Adresse de livraison actuelle',
     'TABLE_HEADING_ADDRESS_BOOK_ENTRIES' => '... ou choisissez parmi les entrées du carnet d\'adresses',
     'TITLE_PLEASE_SELECT' => 'Modifiez l\'adresse de livraison pour cette commande',
@@ -12,7 +11,6 @@ $define = [
     'TEXT_CREATE_NEW_SHIPPING_ADDRESS' => 'Veuillez utiliser le formulaire suivant pour créer une nouvelle adresse de livraison à utiliser pour cette commande.',
     'TITLE_CONTINUE_CHECKOUT_PROCEDURE' => '<strong>Continuer</strong>',
     'TEXT_CONTINUE_CHECKOUT_PROCEDURE' => '- vers la méthode de livraison.',
-    //'SET_AS_PRIMARY' => 'Établir en tant qu\'adresse principale',
 ];
 
 return $define;
