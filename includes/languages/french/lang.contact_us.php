be5ca3658f769b50424e365b9eac033afc81f6fe
alasfarg <44900615+alasfarg@users.noreply.github.com>
2024-04-11 10:57:31 +0200

    Update lang.contact_us.php

--- includes/languages/french/lang.contact_us.php
+++ includes/languages/french/lang.contact_us.php
@@ -3,15 +3,7 @@ $define = [
     'HEADING_TITLE' => 'Nous Contacter',
     'NAVBAR_TITLE' => 'Nous Contacter',
     'TEXT_SUCCESS' => 'Votre message a été envoyé avec succès.',
-    'EMAIL_SUBJECT' => 'Demande de renseignements du site Web '.STORE_NAME,
-    //'ENTRY_NAME' => 'Nom complet: ',
-    //'ENTRY_EMAIL' => 'Adresse e-mail: ',
-    //'ENTRY_TELEPHONE' => 'Numéro de téléphone:',
-    //'ENTRY_ENQUIRY' => 'Message: ',
-    //'SEND_TO_TEXT' => 'Envoyer l\'e-mail à: ',
-    //'ENTRY_EMAIL_NAME_CHECK_ERROR' => 'Désolé, avez-vous bien saisi votre nom ? Notre système requiert un minimum de '.ENTRY_FIRST_NAME_MIN_LENGTH.' caractères. Veuillez réessayer.',
-    //'ENTRY_EMAIL_CONTENT_CHECK_ERROR' => 'Avez-vous oublié de taper votre message ? Nous aimerions vous entendre. Vous pouvez écrire vos commentaires dans la zone de texte ci-dessous.',
-    //'NOT_LOGGED_IN_TEXT' => 'Non connecté',
+    'EMAIL_SUBJECT' => 'Demande de renseignements du site Web ' . STORE_NAME,
 ];
 
 return $define;
